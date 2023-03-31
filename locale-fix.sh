0ea8ff5b6e4e21d8d4a489ede77346d24b228be6
Nullifidian <rick.romig@gmail.com>
2023-03-30 23:47:57 -0400

    locale-fix.sh 0.2.3 - updated & removed -t flag from update-initramfs cmd as it is not shown in man page.

--- locale-fix.sh
+++ locale-fix.sh
@@ -13,14 +13,14 @@
 ## Variables ##
 
 _script=$(basename "$0"); readonly _script
-readonly _version="0.2.2"
-readonly _updated="23 Sep 2022"
+readonly _version="0.2.3"
+readonly _updated="30 Mar 2023"
 
 ## Functions ##
 
 show_intro() {
-  cat << EOF
-$_script changes setting to store locales in individual locale direectories
+  cat << ENDINTRO
+$_script changes the setting to store locales in individual locale direectories
 instead of a single archive file.
 
 During some updates the following warning may occur:
@@ -30,20 +30,20 @@ to see individual locale directories in /usr/lib/locale, but locale-gen is
 configured to generate an archive file by default.
 
 Version: $_version, last updated on $_updated.
-EOF
+ENDINTRO
+}
+
+purge_update() {
+  printf "\nPurging existing locales and changing default setting to not\n"
+  printf "store compiled locale data in a single archive.\n"
+  sudo locale-gen --purge --no-archive
+  printf "Updating the existing initramfs.\n"
+  sudo update-initramfs -u # -t (-t flag isn't listed in man page)
 }
 
 ## Execution ##
 
 user_in_sudo
-
 show_intro
-
-# Remove existing locales & do not store compiled locale data in a single archive
-printf "\nPurging existing locales and changing default setting to not store\n"
-printf "compiled locale data in a single archive.\n"
-sudo locale-gen --purge --no-archive
-
-printf "Updating the existing initramfs.\n"
-sudo update-initramfs -u -t
+purge_update
 exit
