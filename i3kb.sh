88a34d0dcd19639054666ec3e765031e2625f725
Nullifidian <rick.romig@gmail.com>
2023-04-25 12:40:30 -0400

    i3kb.sh 0.1.1: Correct script name in header commments.

--- i3kb.sh
+++ i3kb.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 ##########################################################################
-# Script Name  : i3bindings.sh
+# Script Name  : i3kb.sh
 # Description  : Displays i3 keybindings
 # Dependencies : None
 # Arguments    : None
 # Author       : Copyright © 2023 Richard B. Romig, Mosfanet
 # Email        : rick.romig@gmail.com | rick.romig@mymetronet.net
 # Created      : 16 Apr 2023
-# Last updated : 16 Apr 2023
+# Last updated : 25 Apr 2023
 # Comments     : Install with bindings.list in ~/.config/i3/
 # TODO (Rick)  :
 # License      : GNU General Public License, version 2.0
@@ -29,8 +29,8 @@ fi
 ## Global Variables ##
 
 _script=$(basename "$0"); readonly _script
-readonly _version="0.1.0"
-readonly _updated="16 Apr 2023"
+readonly _version="0.1.1"
+readonly _updated="25 Apr 2023"
 
 ## Functions ##
 
