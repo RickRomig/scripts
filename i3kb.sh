bcd905476be7cc7737dc541c9192322ccb7f0d04
Nullifidian <rick.romig@gmail.com>
2023-05-18 20:11:20 -0400

    i3kb.sh 0.1.2: Added function to copy bindings.list from i3debian repository if not present.

--- i3kb.sh
+++ i3kb.sh
@@ -2,12 +2,12 @@
 ##########################################################################
 # Script Name  : i3kb.sh
 # Description  : Displays i3 keybindings
-# Dependencies : None
+# Dependencies : curl
 # Arguments    : None
 # Author       : Copyright © 2023 Richard B. Romig, Mosfanet
 # Email        : rick.romig@gmail.com | rick.romig@mymetronet.net
 # Created      : 16 Apr 2023
-# Last updated : 25 Apr 2023
+# Last updated : 18 May 2023
 # Comments     : Install with bindings.list in ~/.config/i3/
 # TODO (Rick)  :
 # License      : GNU General Public License, version 2.0
@@ -29,19 +29,27 @@ fi
 ## Global Variables ##
 
 _script=$(basename "$0"); readonly _script
-readonly _version="0.1.1"
-readonly _updated="25 Apr 2023"
+readonly _version="0.1.2"
+readonly _updated="18 May 2023"
+readonly i3_dir=$HOME"/.config/i3"
 
 ## Functions ##
 
+copy_bindings() {
+	local list="$1"
+	local gitea_url="http://192.168.0.16:3000/Nullifidian/i3debian/raw/branch/main/.config/i3"
+	curl -so "$i3_dir/$list" "$gitea_url/$list"
+}
+
 show_bindings() {
-	local binding_path=$HOME"/.config/i3"
-	local binding_list="bindings.list"
-	(printf "Binding|Action|Binding|Action\n"; less "$binding_path/$binding_list") | column -ts "|"
+	local bindings="bindings.list"
+	[[ -f "$i3_dir/$bindings" ]] || copy_bindings "$bindings"
+	(printf "Binding|Action|Binding|Action\n"; less "$i3_dir/$bindings") | column -ts "|"
 }
 
 ## Execution ##
 
+check_package curl
 clear
 printf "%s v%s (%s)\n" "$_script" "$_version" "$_updated"
 show_bindings
