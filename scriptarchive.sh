e6e6d745115d5c3f86d3e00deda29f89414b6ee6
Nullifidian <rick.romig@gmail.com>
2023-11-04 09:10:10 -0400

    scriptarchive.sh 0.3.0 - Changed from shell script to bash script. Replaced zip with tar to creat a tarball archive instead of a zip archive. Delete archives older than 2 years instead of 1.5

--- scriptarchive.sh
+++ scriptarchive.sh
@@ -1,14 +1,14 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 #############################################################################
 # Script Name  : scriptarchive.sh
-# Description  : Makes a dated archive of shell scripts in ~/bin
-# Dependencies : zip
+# Description  : Makes a dated tarball archive of shell scripts in ~/bin
+# Dependencies : None
 # Arguments    : None
 # Author       : Copyright (C) 2020 Richard B. Romig, 20 Jan 2020
 # Email        : rick.romig@gmail.com | rick.romig@mymetronet.net
 # Comments     : Includes all subdirectories
 #              : Schedule with user's crontab from ~/.local/bin
-# Last updated : version 0.2.17, 31 May 2023
+# Last updated : 04 Nov 2023 (version 0.3.0)
 # TODO (Rick)  :
 # License      : GNU General Public License, version 2.0
 #############################################################################
@@ -16,8 +16,7 @@ set -e
 
 ## Variables ##
 
-src_dir=$HOME"/bin"
-archive="$(date +%y%m%d-scripts)"
+archive="$(date +%y%m%d)-scripts.tar.gz"
 arc_dir=$HOME"/Downloads/archives/scripts"
 log_dir=$HOME"/.local/share/logs"
 log_file="script-archive.log"
@@ -26,14 +25,15 @@ err_log="error.log"
 ## Execution ##
 
 # Create archive and log directories if they don't exist
-[ -d "$arc_dir" ] || mkdir -p "$arc_dir"
-[ -d "$log_dir" ] || mkdir -p "$log_dir"
+[[ -d "$arc_dir" ]] || mkdir -p "$arc_dir"
+[[ -d "$log_dir" ]] || mkdir -p "$log_dir"
 
 # Create archive of the ~/bin directory and write results to log file
 rm -f "$arc_dir"/Script\ Archive* > /dev/null 2>&1
 {
   printf "%s|" "$(date '+%a|%F|%R')"
-  if /usr/bin/zip -rq "$arc_dir/$archive" "$src_dir" -x "$src_dir/.git/*" 2> "$arc_dir/$err_log"; then
+  if /usr/bin/tar --exclude='.git' -hcpzf "$arc_dir/$archive" -C "$HOME" bin 2> "$arc_dir/$err_log"
+  then
     printf "successful\n"
     touch "$arc_dir/Script Archive successful - $(date +%F)"
   else
@@ -46,9 +46,9 @@ rm -f "$arc_dir"/Script\ Archive* > /dev/null 2>&1
 
 # Remove oldest log entry if more than 30 entries
 log_len=$(wc -l "$log_dir/$log_file" | cut -d' ' -f1)
-[ "$log_len" -gt 30 ] && sed -i '1d' "$log_dir/$log_file"
-# Remove archives older than one and a half years (550 days).
-find "$arc_dir"/ -maxdepth 1 -type f -name "*.zip" -mtime +550 -exec rm {} +
-# Removes empty error log
-[ -s "$arc_dir/$err_log" ] || rm -f "$arc_dir/$err_log"
+[[ "$log_len" -gt 30 ]] && sed -i '1d' "$log_dir/$log_file"
+# Remove archives older than two years (730 days).
+find "$arc_dir"/ -maxdepth 1 -type f -mtime +730 -exec rm {} +
+# Remove empty error log
+[[ -s "$arc_dir/$err_log" ]] || rm -f "$arc_dir/$err_log"
 exit
