a8d4bfa335bfe639b64dd21f3cd6c1cf34eddb06
Nullifidian <rick.romig@gmail.com>
2023-11-04 11:57:00 -0400

    hb-archive.sh 1.3.3 - Using find and tar to creat archive of .bak file from 2 months previous. Leaving older archive name and zip command until after script runs again. Tested well in test scripts.

--- hb-archive.sh
+++ hb-archive.sh
@@ -6,12 +6,14 @@
 # Arguments    : none
 # Author       : Copyright (C) 2020, Richard B. Romig, 21 January 2020
 # Email        : rick.romig@gmail.com | rick.romig@mymetronet.net
-# Version      : Version 1.3.2
+# Version      : Version 1.3.3
 # Last updated : 01 Nov 2023
 # Comments     : Run from user's crontab to run on the 1st of the month
 #              : to archive 2nd month previous. (1 May archives March files)
 # License      : GNU General Public License, version 2.0
 ###############################################################################
+# shellcheck disable=SC2038
+# SC2038 (warning): Use -print0/-0 or -exec + to allow for non-alphanumeric filenames. (find command)
 
 # Variables
 ref_date=$(date -d "$(date +%Y-%m-01) - 1 month" +%m%d%Y)
@@ -20,7 +22,8 @@ log_date=$(date '+%a|%F|%R')
 arc_dir=$HOME"/Downloads/archives/homebank"
 hb_dir=$HOME"/Documents/HomeBank"
 log_dir=$HOME"/.local/share/logs"
-archive="$arc_date-backup.zip"
+# archive="$arc_date-backup.zip"
+archive="$arc_date-backup.tar.gz"
 cur_hb_bu="hb-bu-$(date +%F).tar.gz"
 log_file="HomeBank-archive.log"
 err_log="HomeBank-error.log"
@@ -35,7 +38,8 @@ rm -f "$arc_dir"/HomeBank\ Archive* > /dev/null 2>&1
 # Archive the .bak files for the 2nd month prior & write success/failure to log.
 {
   printf "%s|%s|" "$log_date" "$arc_date"
-  if zip -qmtt "$ref_date" "$arc_dir/$archive" "$hb_dir"/*.bak 2> "$arc_dir/$err_log"
+  # if zip -qmtt "$ref_date" "$arc_dir/$archive" "$hb_dir"/*.bak 2> "$arc_dir/$err_log"
+  if find "$hb_dir"/  ! -newermt "$ref_date" | xargs tar -czpf "$arc_dir/$archive" -C "$HOME/Documents" HomeBank --remove-files 2> "$arc_dir/$err_log"
   then
     printf "successful\n"
     touch "$arc_dir/HomeBank Archive successful ($(date +%F))"
