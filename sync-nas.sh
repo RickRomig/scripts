670fbf0ee0b897701d685f57077daa2c0fc8907b
Nullifidian <rick.romig@gmail.com>
2023-11-19 23:27:30 -0500

    Initial commit. Script using rsync to copy archives to NAS.

---
+++ sync-nas.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+##########################################################################
+# Script Name  : sync-nas.sh
+# Description  : Use rsync to copy filed from ~/Downloads/archivies to NADS.
+# Dependencies : rsynch
+# Arguments    : None
+# Author       : Copyright © 2023 Richard B. Romig, Mosfanet
+# Email        : rick.romig@gmail.com | rick.romig@mymetronet.net
+# Created      : 19 Nov 2023
+# Last updated : 19 Nov 2023
+# Comments     : Run as a user cron job on hp-800g2-sff (#10)
+# TODO (Rick)  :
+# License      : GNU General Public License, version 2.0
+##########################################################################
+
+rsync -aq --delete "$HOME"/Downloads/archives/finance/ rick@192.168.0.4:archives/finance/ 2>/dev/null
+rsync -aq --delete "$HOME"/Downloads/archives/gitea/ rick@192.168.0.4:archives/gitea/ 2>/dev/null
+rsync -aq --delete "$HOME"/Downloads/archives/gitea-repo/ rick@192.168.0.4:archives/gitea-repo/ 2>/dev/null
+rsync -aq --delete "$HOME"/Downloads/archives/projects-repo/ rick@192.168.0.4:archives/projects-repo/ 2>/dev/null
+rsync -aq --delete "$HOME"/Downloads/archives/scripts/ rick@192.168.0.4:archives/scripts/ 2>/dev/null
+rsync -aq --delete "$HOME"/Downloads/archives/homebank/ rick@192.168.0.4:archives/homebank/ 2>/dev/null
+rsync -aq --delete "$HOME"/Downloads/archives/homepage/ rick@192.168.0.4:archives/homepage/ 2>/dev/null
+exit
