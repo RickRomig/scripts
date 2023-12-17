9a41f89cdb3685bf704f429d06b3e28a60dfe864
Nullifidian <rick.romig@gmail.com>
2023-12-17 15:38:14 -0500

    Changed finance backup to include entire Finance directory, excluding the Archives directory.

--- fin-backup.sh
+++ fin-backup.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
 ##########################################################################
 # Script Name  : fin-backup.sh
-# Description  : Incremental backups of Finance directories
+# Description  : Incremental backups of Finance & HomeBank directories
 # Dependencies : none
 # Arguments    : none
 # Author       : Copyright © 2023 Richard B. Romig, Mosfanet
 # Email        : rick.romig@gmail.com | rick.romig@mymetronet.net
 # Created      : 28 Oct 2023
-# Last updated : 19 Nov 2023 (Version 0.1.2)
-# Comments     : Run as a daily cron job on finance system
+# Last updated : 17 Dec 2023 (Version 0.1.3)
+# Comments     : Run as a daily cron job on the finance system
+#              : Excludes ~/Documents/Finance/Archives directory
 # TODO (Rick)  :
 # License      : GNU General Public License, version 2.0
 ##########################################################################
@@ -29,7 +30,8 @@ if [[ "$day" == "Sun" ]]; then
   find "$arc_dir" -mtime +85 -delete
 fi
 
-tar -cpzf "$arc_dir/$archive" -g "$arc_dir/$snar" "$HOME"/Documents/Finance/CY2022 "$HOME"/Documents/Finance/CY2023 "$HOME"/Documents/HomeBank
+# tar -cpzf "$arc_dir/$archive" -g "$arc_dir/$snar" "$HOME"/Documents/Finance/CY2022 "$HOME"/Documents/Finance/CY2023 "$HOME"/Documents/HomeBank
+tar --exclude='Archives' -cpzf "$arc_dir/$archive" -g "$arc_dir/$snar" "$HOME"/Documents/Finance "$HOME"/Documents/HomeBank
 
 rsync -aq --delete "$HOME"/Downloads/archives/finance/ 192.168.0.10:Downloads/archives/finance/
 
