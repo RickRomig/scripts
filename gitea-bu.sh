60389327cf9d6ed3121e35f1aea0e5c538cefe61
Nullifidian <rick.romig@gmail.com>
2024-01-08 14:44:36 -0500

    Commented out sngz lines, appears transition from sngz to snar went well.

--- gitea-bu.sh
+++ gitea-bu.sh
@@ -7,7 +7,7 @@
 # Author       : Copyright © 2023 Richard B. Romig, Mosfanet
 # Email        : rick.romig@gmail.com | rick.romig@mymetronet.net
 # Created      : 26 Oct 2023
-# Last updated : 07 Jan 2024 (Version 0.1.6)
+# Last updated : 08 Jan 2024 (Version 0.1.7)
 # Comments     : Run as a daily cron job on the main system.
 # TODO (Rick)  :
 # License      : GNU General Public License, version 2.0
@@ -19,7 +19,7 @@ set -euo pipefail
 
 day=$(date +%a)
 snar="gitea.snar"
-sngz="gitea.sngz"
+# sngz="gitea.sngz"
 arc_date=$(date +'%y%m%d-%u')
 archive="gitea.$arc_date.tar.gz"
 arc_dir="$HOME/Downloads/archives/gitea-repo"
@@ -29,7 +29,7 @@ arc_dir="$HOME/Downloads/archives/gitea-repo"
 # On Sunday, set up SNAR file for full backup & delete archives older than 3 months.
 if [[ "$day" == "Sun" ]]; then
 	[[ -e "$arc_dir/$snar" ]] && mv "$arc_dir/$snar" "$arc_dir/$snar.$(date --date '7 days ago' +%y%m%d)"
-	[[ -e "$arc_dir/$sngz" ]] && mv "$arc_dir/$sngz" "$arc_dir/$sngz.$(date --date '7 days ago' +%y%m%d)"
+	# [[ -e "$arc_dir/$sngz" ]] && mv "$arc_dir/$sngz" "$arc_dir/$sngz.$(date --date '7 days ago' +%y%m%d)"
   find "$arc_dir" -mtime +90 -delete
 fi
 
