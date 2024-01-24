b0b47e8823e58b860ba2ed2be095fa32495869d5
Nullifidian <rick.romig@gmail.com>
2024-01-24 13:55:47 -0500

    added -q option to rysnc command.

--- gitea-dwm-bu.sh
+++ gitea-dwm-bu.sh
@@ -7,7 +7,7 @@
 # Author       : Copyright © 2024 Richard B. Romig, Mosfanet
 # Email        : rick.romig@gmail.com | rick.romig@mymetronet.net
 # Created      : 17 Jan 2024
-# Last updated : 18 Jan 2024 Version 1.0.2
+# Last updated : 24 Jan 2024 Version 1.0.3
 # Comments     :
 # TODO (Rick)  :
 # License      : GNU General Public License, version 2.0
@@ -46,6 +46,6 @@ daily_bu
 [[ "$dom" -eq 1 ]] && monthly_bu
 
 # Sync archives to Gitea server
-rsync -a --delete "$HOME"/Downloads/archives/gitea/ rick@192.168.0.16:Downloads/archives/gitea/
+rsync -aq --delete "$HOME"/Downloads/archives/gitea/ rick@192.168.0.16:Downloads/archives/gitea/
 
 exit
