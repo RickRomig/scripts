27f6a2bcb8d40db865a3716c8d2eaf04e5650371
Nullifidian <rick.romig@gmail.com>
2024-04-06 12:57:59 -0400

    empty-trash.sh - added lines to separate trash contents from displayed messages. Changed to new version format.

--- empty-trash.sh
+++ empty-trash.sh
@@ -7,7 +7,7 @@
 # Author       : Copyright © 2023, Richard B. Romig, Mosfanet
 # Email        : rick.romig@gmail.com | rick.romig@mymetronet.com
 # Created      : 21 Nov 2023
-# Last updated : 18 Mar 2024 Version 0.1.5
+# Last updated : 06 Apr 2024 Version 1.6.24097
 # Comments     : Run as a user cron job.
 #              : Trash directory does not exist until a file is moved to the trash.
 #              : Tested with Debian 11/12, LMDE 6, Mint 21.x, MX Linux 23.1, BunsenLabs 11.
@@ -21,11 +21,11 @@ readonly log_file="trash.log"
 
 empty_trash() {
 	if [[ $(find "$trash_dir/files" -type f | wc -l) -gt 0 ]]; then
-		printf "Trash contents:\n"
+		printf "Trash contents:\n---------------\n"
 		find "$trash_dir/files" -type f -exec basename {} \;
 		rm -rf "$trash_dir"/files/*
 		rm -f "$trash_dir"/info/*
-		printf "Trash emptied.\n"
+		printf "--------------\nTrash emptied.\n"
 	else
 		printf "No trash to empty.\n"
 	fi
