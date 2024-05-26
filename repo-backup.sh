fa8549a04c819fd1cf282f5afa49d68a0256e091
Nullifidian <rick.romig@gmail.com>
2024-05-25 23:26:52 -0400

    repo-backup.sh - renamed from gitea-backup.sh, rsync gitea & Projects directories with Gitea server (16).

--- repo-backup.sh
+++ repo-backup.sh
@@ -1,19 +1,20 @@
 #!/usr/bin/env bash
 ##########################################################################
-# Script Name  : gitea-backup.sh
+# Script Name  : repo-backup.sh
 # Description  : rsync backup of gitea archives to gitea server
 # Dependencies : rync
 # Arguments    : none
 # Author       : Copyright © 2023 Richard B. Romig, Mosfanet
 # Email        : rick.romig@gmail.com | rick.romig@mymetronet.net
 # Created      : 16 Sep 2023
-# Last updated : 24 Jan 2024 Version 1.0.3
+# Last updated : 25 May 2024 Version 1.3.24146
 # Comments     :
 # TODO (Rick)  :
 # License      : GNU General Public License, version 2.0
 ##########################################################################
 
-set -euo pipefail
+set -eu
 
-rsync -aq --delete "$HOME"/Downloads/archives/gitea/ rick@192.168.0.16:Downloads/archives/gitea/
+rsync -aq --delete "$HOME"/gitea/ rick@192.168.0.16:gitea/
+rsync -aq --delete "$HOME"/Projects/ rick@192.168.0.16:Projects/
 exit
