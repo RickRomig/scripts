0bd52fb3e2964a1f2068c00660165be55add316d
Nullifidian <rick.romig@gmail.com>
2024-06-28 12:00:13 -0400

    wifi-up.sh - created functions to get wifi interface and IP address; added conditional statements.

--- wifi-up.sh
+++ wifi-up.sh
@@ -1,20 +1,20 @@
 #!/usr/bin/env bash
 ###############################################################################
 # Script Name  : wifi-up.sh
-# Description  : Checks wifi connection & if down, bring it up if toggled on.
+# Description  : Checks wifi connection & if down, bring it up.
 # Dependencies : none
 # Arguments    : none
-# Author       : Richard B. Romig, 21 Jan 2022
-# Email        : rick.romig@gmail.com
+# Author       : Copyright © 2022, Richard B. Romig, LudditeGeek@Mosfanet
+# Email        : rick.romig@gmail.com | rick.romig@mymetronet.net
+# Created      : 21 Jan 2022
+# Updated      : 28 Jun 2024
 # Comments     :
-# Last updated : 21 Jan 2022
 # TODO (Rick)  :
 # License      : GNU General Public License, version 2.0
 ###############################################################################
 
 ## Shellcheck Directives ##
 # shellcheck source=/home/rick/bin/functionlib
-# shellcheck disable=SC1091
 
 ## Source function library ##
 
@@ -28,35 +28,42 @@ fi
 ## Variables ##
 
 _script=$(basename "$0"); readonly _script
-readonly _version="0.1.2"
-readonly _updated="04 Apr 2023"
+readonly _version="2.0.24180"
 
-echo "$_script v$_version ($_updated)"
+## Functions ##
 
-# check_package iwgetid
-# iw ?
+get_wifi_interface() {
+  local wifi_int
+  wifi_int=$(ip addr show | awk '/: wl/ {print $2}' | sed 's/:$//')
+  echo "$wifi_int"
+}
 
-# bring up wireless interface
+get_ip_address() {
+  local wifi_ip wifi_int
+  wifi_int="$1"
+  wifi_ip=$(/sbin/ip -o -4 addr show | awk -v name="$wifi_int" '$0~name {print $4}')
+  echo "$wifi_ip"
+}
 
-# Extract wireless interface
-# wifi_int=$(/usr/bin/nmcli dev | awk '/wifi/ {print $1}')
-wifi_int=$(ip addr show | awk '/: wl/ {print $2}' | sed 's/:$//')
-# wifi_int=$(iwgetid | awk '{ print $1 }')
-[ -z "$wifi_int" ] && die "No wireless interface detected."
+## Execution ##
 
-wifi_ip=$(/sbin/ip -o -4 addr show | awk -v name="$wifi_int" '$0~name {print $4}')
-[ -z "$wifi_ip" ] && echo "No IP address found. Is $wifi_int up?"
+box "$_script v$_version" "*"
+echo ""
 
-# Check if interface is down and bring up if not
-/sbin/ip link show "$wifi_int" | grep 'DOWN' && sudo /sbin/ip link set "$wifi_int" up
+wifi_int=$(get_wifi_interface)
+[[ "$wifi_int" ]] || die "No wireless interface found." 1
 
-# Check again. If down, have user check if device is toggled on
-/sbin/ip link show "$wifi_int" | grep 'DOWN' && die "Make sure WiFi is toggled on."
-
-# Show wireless IP address
-wifi_ip=$(/sbin/ip -o -4 addr show | awk -v name="$wifi_int" '$0~name {print $4}')
-if [ -n "$wifi_ip" ]; then
-  leave "IP = $wifi_ip"
+wifi_ip=$(get_ip_address "$wifi_int")
+if [[ "$wifi_ip" ]]; then
+  leave "Wireless IP = $wifi_ip"
 else
-  die "No IP address found. Begin troubleshooting."
+  echo "No wireless IP address found. Is $wifi_int up?"
+  # Check if interface is down and bring up if not
+  /sbin/ip link show "$wifi_int" | grep 'DOWN' && sudo /sbin/ip link set "$wifi_int" up
+  # Check again. If down, have user check if device is toggled on
+  /sbin/ip link show "$wifi_int" | grep 'DOWN' && die "Make sure WiFi is toggled on." 1
+  # Show wireless IP address
+  wifi_ip=$(get_ip_address "$wifi_int")
+  [[ "$wifi_ip" ]] && leave "Wireless IP = $wifi_ip"
 fi
+die "No IP address found. Begin troubleshooting." 1
