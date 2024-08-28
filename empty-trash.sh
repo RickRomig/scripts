f0e85d2d957bdcb970d03da73b1851ec0bd2b4be
Nullifidian <rick.romig@gmail.com>
2024-08-28 09:30:29 -0400

    empty-trash.sh - added function to check if libglib2.0-bin is installed. If not, error message is printed to log file. Added gio list to list files in trash & alt method if not supported.

--- empty-trash.sh
+++ empty-trash.sh
@@ -2,12 +2,12 @@
 ###############################################################################
 # Script Name  : empty-trash.sh
 # Description  : Empty the local trash directory
-# Dependencies : none
+# Dependencies : gio (libglib2.0-bin)
 # Arguments    : none
 # Author       : Copyright © 2023, Richard B. Romig, Mosfanet
 # Email        : rick.romig@gmail.com | rick.romig@mymetronet.com
 # Created      : 21 Nov 2023
-# Last updated : 22 Aug 2024 Version 1.7.24237
+# Last updated : 28 Aug 2024 Version 2.0.24241
 # Comments     : Run as a user cron job.
 #              : Trash directory does not exist until a file is moved to the trash.
 #              : Tested with Debian 11/12, LMDE 6, Mint 21.x, MX Linux 23.1, BunsenLabs 11.
@@ -19,13 +19,16 @@ readonly trash_dir="$HOME/.local/share/Trash"
 readonly log_dir="$HOME/.local/share/logs"
 readonly log_file="trash.log"
 
+check_gio() {
+	dpkg -l libglib2.0-bin >/dev/null 2>&1 && return 0 || return 1
+}
+
 empty_trash() {
 	if [[ $(find "$trash_dir/files" -type f | wc -l) -gt 0 ]]; then
 		printf "\nTrash contents:\n---------------\n"
-		find "$trash_dir/files" -type f -exec basename {} \;
+		# find "$trash_dir/files" -type f -exec basename {} \;
+		gio trash --list 2>/dev/null || gio list -h "$trash_dir"/files
 		gio trash --empty
-		# rm -rf "$trash_dir"/files/*
-		# rm -f "$trash_dir"/info/*
 		printf "\nTrash emptied.\n"
 	else
 		printf "\nNo trash to empty.\n"
@@ -36,10 +39,14 @@ empty_trash() {
 
 {
 	printf "Date: %s \n" "$(date '+%F %R')"
-	if [[ -d "$trash_dir" ]]; then
-		empty_trash
+	if check_gio; then
+		if [[ -d "$trash_dir" ]]; then
+			empty_trash
+		else
+	    printf "\nTrash directory does not exist.\n"
+		fi
 	else
-    printf "\nTrash directory does not exist.\n"
+		printf "\n\e[91mERROR:\e[0m libglib2.0-bin not installed!\n"
 	fi
 } > "$log_dir/$log_file" 2>&1
 exit
