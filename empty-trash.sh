9ff473ada8a3c8f63239a9a83ffba7dfaa64a611
Nullifidian <rick.romig@gmail.com>
2024-09-01 12:00:34 -0400

    empty-trash.sh - added -f flag to rm command in empty_trash()

--- empty-trash.sh
+++ empty-trash.sh
@@ -7,7 +7,7 @@
 # Author       : Copyright © 2023, Richard B. Romig, Mosfanet
 # Email        : rick.romig@gmail.com | rick.romig@mymetronet.com
 # Created      : 21 Nov 2023
-# Last updated : 30 Aug 2024 Version 2.3.24243
+# Last updated : 01 Sep 2024 Version 2.3.24245
 # Comments     : Run as a user cron job.
 #              : Trash directory does not exist until a file is moved to the trash.
 #              : Tested with Debian 11/12, LMDE 6, Mint 21.x, Mint 22, MX Linux 23.1, BunsenLabs 11.
@@ -24,7 +24,7 @@ empty_trash() {
 		printf "\nTrash contents:\n---------------\n"
 		tree -a "$trash_dir"
 		find "$trash_dir"/files -mindepth 1 -type f,d -exec rm -rf {} +
-		rm "$trash_dir"/info/*
+		rm -f "$trash_dir"/info/*
 		printf "\nTrash emptied.\n"
 		tree -a "$trash_dir"
 	else
