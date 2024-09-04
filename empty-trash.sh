9ac25339469d059dc973c945bb1504ecbb3c0933
Nullifidian <rick.romig@gmail.com>
2024-09-04 00:04:39 -0400

    empty-trash.sh - uses trash-list and trash-empty from trash-cli package.

--- empty-trash.sh
+++ empty-trash.sh
@@ -2,12 +2,12 @@
 ###############################################################################
 # Script Name  : empty-trash.sh
 # Description  : Empty the local trash directory
-# Dependencies : trash-cli tree 'sudo apt install trash-cli tree'
+# Dependencies : trash-cli 'sudo apt install trash-cli'
 # Arguments    : none
 # Author       : Copyright © 2023, Richard B. Romig, Mosfanet
 # Email        : rick.romig@gmail.com | rick.romig@mymetronet.com
 # Created      : 21 Nov 2023
-# Last updated : 03 Sep 2024 Version 2.6.24247
+# Last updated : 03 Sep 2024 Version 3.0.24247
 # Comments     : Run as a user cron job.
 #              : Trash directory does not exist until a file is moved to the trash.
 #              : Tested with Debian 11/12, LMDE 6, Mint 21.x, Mint 22, MX Linux 23.1, BunsenLabs 11.
@@ -20,17 +20,11 @@ readonly log_dir="$HOME/.local/share/logs"
 readonly log_file="trash.log"
 
 empty_trash() {
-	if [[ $(find "$trash_dir"/info -type f | wc -l) -gt 0 ]]; then
+	if [[ $(/usr/bin/trash-list | wc -l) -gt 0 ]]; then
 		printf "\nTrash contents:\n---------------\n"
-		tree -a "$trash_dir"
-		if [[ -x usr/bin/trash-empty ]]; then
-			/usr/bin/trash-empty
-		else
-			find "$trash_dir"/files -mindepth 1 -type f,d -exec rm -rf {} +
-			rm -f "$trash_dir"/info/*
-		fi
-		printf "\nTrash emptied.\n\n"
-		tree -a "$trash_dir"
+		/usr/bin/trash-list
+		/usr/bin/trash-empty 7
+		/usr/bin/trash-list
 	else
 		printf "\nNo trash to empty.\n"
 	fi
@@ -40,7 +34,9 @@ empty_trash() {
 
 {
 	printf "Date: %s \n" "$(date '+%F %R')"
-	if [[ -d "$trash_dir" ]]; then
+	if ! dpkg -l trash-cli > /dev/null 2>&1; then
+		printf "trash-cli package is not installed.\n"
+	elif [[ -d "$trash_dir" ]]; then
 		empty_trash
 	else
 		printf "\nTrash directory does not exist.\nWill be created when a file is moved to the trash.\n"
