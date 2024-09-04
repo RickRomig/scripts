d4a5f2cf156bcb92d81ebc007a762b388256ddb7
Nullifidian <rick.romig@gmail.com>
2024-09-04 09:04:23 -0400

    empty-trash.sh - added dialog and if-else to determin actions base on trash left behind.

--- empty-trash.sh
+++ empty-trash.sh
@@ -7,10 +7,9 @@
 # Author       : Copyright © 2023, Richard B. Romig, Mosfanet
 # Email        : rick.romig@gmail.com | rick.romig@mymetronet.com
 # Created      : 21 Nov 2023
-# Last updated : 03 Sep 2024 Version 3.0.24247
+# Updated      : 04 Sep 2024 Version 3.1.24248
 # Comments     : Run as a user cron job.
 #              : Trash directory does not exist until a file is moved to the trash.
-#              : Tested with Debian 11/12, LMDE 6, Mint 21.x, Mint 22, MX Linux 23.1, BunsenLabs 11.
 # TODO (Rick)  :
 # License      : GNU General Public License, version 2.0
 ###############################################################################
@@ -18,13 +17,20 @@
 readonly trash_dir="$HOME/.local/share/Trash"
 readonly log_dir="$HOME/.local/share/logs"
 readonly log_file="trash.log"
+last_week=$(date -d "$(date) - 7 days" +%F)
 
 empty_trash() {
 	if [[ $(/usr/bin/trash-list | wc -l) -gt 0 ]]; then
 		printf "\nTrash contents:\n---------------\n"
 		/usr/bin/trash-list
+		printf "Removing trash before %s...\n" "$last_week"
 		/usr/bin/trash-empty 7
-		/usr/bin/trash-list
+		if [[ $(/usr/bin/trash-list | wc -l) -gt 0 ]]; then
+			printf "Trash after %s...\n" "$last_week"
+			/usr/bin/trash-list
+		else
+			printf "All trash removed.\n"
+		fi
 	else
 		printf "\nNo trash to empty.\n"
 	fi
