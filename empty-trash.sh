e850cc431343f58c5ae9af991ac47a43c3500b7d
Nullifidian <rick.romig@gmail.com>
2024-09-28 09:56:43 -0400

    empty-trash.sh - created main() for code previously outside functions.

--- empty-trash.sh
+++ empty-trash.sh
@@ -7,16 +7,12 @@
 # Author       : Copyright © 2023, Richard B. Romig, Mosfanet
 # Email        : rick.romig@gmail.com | rick.romig@mymetronet.com
 # Created      : 21 Nov 2023
-# Updated      : 27 Sep 2024 Version 4.0.24271
+# Updated      : 28 Sep 2024, Version 4.1.24272
 # Comments     : Run as a user cron job.
 #              : Trash directory does not exist until a file is moved to the trash.
 # TODO (Rick)  :
 # License      : GNU General Public License, version 2.0
 ###############################################################################
-
-readonly trash_dir="$HOME/.local/share/Trash"
-readonly log_dir="$HOME/.local/share/logs"
-readonly log_file="trash.log"
 
 trash() {
 	local count; count=$(/usr/bin/trash-list | wc -l)
@@ -41,16 +37,24 @@ empty_trash() {
 	fi
 }
 
-[[ -d "$log_dir" ]] || mkdir -p "$log_dir"
+main() {
+	local trash_dir log_dir log_file
+	trash_dir="$HOME/.local/share/Trash"
+	log_dir="$HOME/.local/share/logs"
+	log_file="trash.log"
+	[[ -d "$log_dir" ]] || mkdir -p "$log_dir"
 
-{
-	printf "Date: %s \n" "$(date '+%F %R')"
-	if ! dpkg -l trash-cli > /dev/null 2>&1; then
-		printf "trash-cli package is not installed.\n"
-	elif [[ -d "$trash_dir" ]]; then
-		empty_trash
-	else
-		printf "\nTrash directory does not exist.\nWill be created when a file is moved to the trash.\n"
-	fi
-} > "$log_dir/$log_file" 2>&1
-exit
+	{
+		printf "Date: %s \n" "$(date '+%F %R')"
+		if ! dpkg -l trash-cli > /dev/null 2>&1; then
+			printf "trash-cli package is not installed.\n"
+		elif [[ -d "$trash_dir" ]]; then
+			empty_trash
+		else
+			printf "\nTrash directory does not exist.\nWill be created when a file is moved to the trash.\n"
+		fi
+	} > "$log_dir/$log_file" 2>&1
+	exit
+}
+
+main
