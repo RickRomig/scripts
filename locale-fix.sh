9f0b794d628f3218da19b70a025cbd035c19e293
Nullifidian <rick.romig@gmail.com>
2024-10-02 21:10:39 -0400

    locale-fix.sh - added main()

--- locale-fix.sh
+++ locale-fix.sh
@@ -7,7 +7,7 @@
 # Author       : Richard Romig
 # Email        : rick.romig@gmail.com
 # Created      :
-# Updated      : 06 Jun 2024
+# Updated      : 02 Oct 2024
 # Comment      :
 # License      : GNU General Public License, version 2.0
 #########################################################################
@@ -24,17 +24,19 @@ else
   exit 1
 fi
 
-## Variables ##
+set -eu
 
-_script=$(basename "$0"); readonly _script
-readonly _version="2.3.24158"
-readonly _updated="06 Jun 2024"
+## Global Variables ##
+
+script=$(basename "$0"); readonly script
+readonly version="2.4.24276"
 
 ## Functions ##
 
 show_intro() {
-  cat << ENDINTRO
-$_script changes the setting to store locales in individual locale direectories
+  local updated="02 Oct 2024"
+  cat << _INTRO_
+$script changes the setting to store locales in individual locale direectories
 instead of a single archive file.
 
 During some updates the following warning may occur:
@@ -43,21 +45,25 @@ The problem is that /usr/share/initramfs-tools/hooks/root_locale is expecting
 to see individual locale directories in /usr/lib/locale, but locale-gen is
 configured to generate an archive file by default.
 
-Version: $_version, last updated on $_updated.
-ENDINTRO
+Version: $version, last updated on $updated.
+_INTRO_
 }
 
 purge_update() {
-  printf "\nPurging existing locales and changing default setting to not\n"
-  printf "store compiled locale data in a single archive.\n"
+  printf "\nPurging existing locales and changing the default setting\n"
+  printf "to not store compiled locale data in a single archive.\n"
   sudo locale-gen --purge --no-archive
   printf "Updating the existing initramfs.\n"
   sudo update-initramfs -u # -t (-t flag isn't listed in man page)
 }
 
+main() {
+  sudo_login 2
+  show_intro
+  purge_update
+  leave "$script v$version"
+}
+
 ## Execution ##
 
-sudo_login 2
-show_intro
-purge_update
-exit
+main "$@"
