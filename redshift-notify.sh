232a27893ac0e8a03175059588c1246d6dbff254
Nullifidian <rick.romig@gmail.com>
2024-10-28 09:35:50 -0400

    redshift-notify.sh - corrected touch command.

--- redshift-notify.sh
+++ redshift-notify.sh
@@ -7,7 +7,7 @@
 # Author       : Copyright © 2024, Richard B. Romig, Mosfanet
 # Email        : rick.romig@gmail.com | rick.romig@mymetronet.com
 # Created      : 28 Oct 2024
-# Last updated : 28 Oct 2024 version 1.0.24301
+# Last updated : 28 Oct 2024 version 1.0.24302
 # Comments     :
 # TODO (Rick)  :
 # License      : GNU General Public License, version 2.0
@@ -27,11 +27,10 @@ elif [[ "$brightness" != "1.0" && -f "$DAYTIME_FILE" ]]; then
 	rm "$DAYTIME_FILE"
 fi
 
-
 if [[ "$brightness" == "1.0" && ! -f "$DAYTIME_FILE" ]]; then
-	touch"$DAYTIME_FILE"
+	touch "$DAYTIME_FILE"
 	notify-send -t 3500 "Redshift" "Changed to day setting"
 elif [[ "$brightness" != "1.0" && ! -f  "$NIGHTIME_FILE" ]]; then
-	touch  "$NIGHTIME_FILE"
+	touch "$NIGHTIME_FILE"
 	notify-send -t 3500 "Redshift" "Changed to night setting"
 fi
