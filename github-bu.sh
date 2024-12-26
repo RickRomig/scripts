b03366c877401d2014df43057fa798884b23a361
Nullifidian <rick.romig@gmail.com>
2024-12-25 19:58:27 -0500

    github-bu.sh - implemented main() and sunday_actions()

--- github-bu.sh
+++ github-bu.sh
@@ -7,7 +7,7 @@
 # Author       : Copyright © 2023 Richard B. Romig, Mosfanet
 # Email        : rick.romig@gmail.com | rick.romig@mymetronet.net
 # Created      : 26 Oct 2023
-# Last updated : 24 Dec 2024 (Version 1.10.24359)
+# Last updated : 25 Dec 2024 (Version 2.0.24360)
 # Comments     : Run as a daily cron job on the main system.
 # TODO (Rick)  :
 # License      : GNU General Public License, version 2.0
@@ -15,23 +15,26 @@
 
 set -eu
 
-## Global Variables ##
+## Functions ##
 
-day=$(date +%a)
-snar="github.snar"
-arc_date=$(date +'%y%m%d-%u')
-archive="github.$arc_date.tar.gz"
-arc_dir="$HOME/Downloads/archives/projects-repo"
+sunday_actions() {
+	local arc_dir="$1"
+	local snar="$2"
+	[[ -e "$arc_dir/$snar" ]] && mv "$arc_dir/$snar" "$arc_dir/$snar.$(date --date '7 days ago' +%y%m%d)"
+	find "$arc_dir" -mtime +91 -delete
+}
+
+main() {
+	local arc_dir archive day snar
+	arc_dir="$HOME/Downloads/archives/projects-repo"
+	archive="github.$(date +'%y%m%d-%u').tar.gz"
+	snar="github.snar"
+	day=$(date +%a)
+	[[ "$day" == "Sun" ]] && sunday_actions "$arc_dir" "$snar"
+	tar -cpzg "$arc_dir/$snar" -f "$arc_dir/$archive" -C "$HOME" Projects
+	exit
+}
 
 ## Execution ##
 
-# On Sunday, rename SNAR file for full backup & delete archives older than 3 months.
-if [[ "$day" == "Sun" ]]; then
-	[[ -e "$arc_dir/$snar" ]] && mv "$arc_dir/$snar" "$arc_dir/$snar.$(date --date '7 days ago' +%y%m%d)"
-  find "$arc_dir" -mtime +91 -delete
-fi
-
-# Incremental backup of GitHub repositories (Projects directory).
-tar -cpzg "$arc_dir/$snar" -f "$arc_dir/$archive" -C "$HOME" Projects
-
-exit
+main "$@"
