319e599e24336033678cb3af3f3297a8519b6014
Nullifidian <rick.romig@gmail.com>
2024-12-25 19:56:37 -0500

    fin-bu.sh - implemented main() and sunday_actions()

--- fin-bu.sh
+++ fin-bu.sh
@@ -7,30 +7,35 @@
 # Author       : Copyright © 2023 Richard B. Romig, Mosfanet
 # Email        : rick.romig@gmail.com | rick.romig@mymetronet.net
 # Created      : 28 Oct 2023
-# Last updated : 23 Nov 2024 (Version 1.7.24328)
+# Last updated : 25 Dec 2024 (Version 2.0.24360)
 # Comments     : Run as a daily cron job
 #              : Excludes ~/Documents/Finance/Archives directory
 # TODO (Rick)  :
 # License      : GNU General Public License, version 2.0
 ##########################################################################
 
-## Global Variables ##
+set -eu
 
-day=$(date +%a)
-snar="finance.snar"
-arc_date=$(date +'%y%m%d-%u')
-archive="finance.$arc_date.tar.gz"
-arc_dir="$HOME/Downloads/archives/finance"
+## Functions ##
+
+sunday_actions() {
+	local arc_dir="$1"
+	local snar="$2"
+	[[ -e "$arc_dir/$snar" ]] && mv "$arc_dir/$snar" "$arc_dir/$snar.$(date --date '7 days ago' +%y%m%d)"
+	find "$arc_dir" -mtime +91 -delete
+}
+
+main() {
+	local arc_dir archive day snar
+	arc_dir="$HOME/Downloads/archives/finance"
+	archive="finance.$(date +'%y%m%d-%u').tar.gz"
+	snar="finance.snar"
+	day=$(date +%a)
+	[[ "$day" == "Sun" ]] && sunday_actions "$arc_dir" "$snar"
+	tar --exclude='Archives' -cpzf "$arc_dir/$archive" -g "$arc_dir/$snar" "$HOME"/Documents/Finance "$HOME"/Documents/HomeBank
+	exit
+}
 
 ## Execution ##
 
-# On Sunday, set up SNAR file for full backup & delete archives older than 3 months.
-if [[ "$day" == "Sun" ]]; then
-	[[ -e "$arc_dir/$snar" ]] && mv "$arc_dir/$snar" "$arc_dir/$snar.$(date --date '7 days ago' +%y%m%d)"
-  find "$arc_dir" -mtime +91 -delete
-fi
-
-# Incremental backup of Finance and HomeBank directories.
-tar --exclude='Archives' -cpzf "$arc_dir/$archive" -g "$arc_dir/$snar" "$HOME"/Documents/Finance "$HOME"/Documents/HomeBank
-
-exit
+main "$@"
