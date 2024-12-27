d26428f5bc49df97d4e2b3c1a0fffe3344533420
Nullifidian <rick.romig@gmail.com>
2024-12-27 09:31:51 -0500

    journal-bu.sh - implemented main() and sunday_actions()

--- journal-bu.sh
+++ journal-bu.sh
@@ -7,36 +7,37 @@
 # Author       : Copyright © 2024 Richard B. Romig, Mosfanet
 # Email        : rick.romig@gmail.com | rick.romig@mymetronet.net
 # Created      : 11 Mar 2024
-# Last updated : 12 Mar 2024
-# Version      : 0.1.0
+# Last updated : 27 Dec 2024
+# Version      : 2.0.24362
 # Comments     : Run as a local cron job.
 # TODO (Rick)  :
 # License      : GNU General Public License, version 2.0
 ##########################################################################
 
-set -euo pipefail
+set -eu
 
-## Global Variables ##
+## Functions ##
 
-day=$(date +%a)
-arc_date=$(date +'%y%m%d-%u')
-arc_dir="$HOME/Downloads/archives/journals"
-archive="journals.$arc_date.tar.gz"
-snar="journal.snar"
+sunday_actions() {
+	local arc_dir="$1"
+	local snar="$2"
+	[[ -e "$arc_dir/$snar" ]] && mv "$arc_dir/$snar" "$arc_dir/$snar.$(date --date '7 days ago' +%y%m%d)"
+	find "$arc_dir" -mtime +91 -delete
+}
+
+main() {
+	local arc_dir archive day snar
+	arc_dir="$HOME/Downloads/archives/journals"
+	archive="journals.$(date +'%y%m%d-%u').tar.gz"
+	snar="journal.snar"
+	day=$(date +%a)
+	[[ -d "$arc_dir" ]] || mkdir -p "$arc_dir"
+	[[ "$day" == "Sun" ]] && sunday_actions "$arc_dir" "$snar"
+	tar -cpzg "$arc_dir/$snar" -f "$arc_dir/$archive" -C "$HOME" Journals
+	rsync -aq --delete "$HOME"/Downloads/archives/journals/ rick@192.168.0.11:/data/archives/journals/
+	exit
+}
 
 ## Execution ##
 
-[[ -d "$arc_dir" ]] || mkdir -p "$arc_dir"
-
-# On Sunday, rename previous snar file and delete archives older than 3 monthss (13 weeks)
-if [[ "$day" == "Sun" ]]; then
-	[[ -e "$arc_dir/$snar" ]] && mv "$arc_dir/$snar" "$arc_dir/$snar.$(date --date '7 days ago' +%y%m%d)"
-  find "$arc_dir" -mtime +91 -delete
-fi
-
-# Full/Incremental backup of personal journals (excludes locked files)
-tar -cpzg "$arc_dir/$snar" -f "$arc_dir/$archive" --exclude='.~lock*' -C "$HOME/Documents" Journals
-
-# Copy archive to HP-6005
-rsync -aq --delete "$HOME"/Downloads/archives/journals/ rick@192.168.0.11:/data/archives/journals/
-exit
+main "$@"
