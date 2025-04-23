a176a691885f71e8fed999266e37f85db3529303
Nullifidian <rick.romig@gmail.com>
2025-04-23 14:22:46 -0400

    cam.sh: created play_game() function; changed location of files to ~/bin/files

--- cam.sh
+++ cam.sh
@@ -7,7 +7,7 @@
 # Author       : Copyright © 2025 Richard B. Romig, Mosfanet
 # Email        : rick.romig@gmail | rick.romig@mymetronet.net
 # Created      : 23 Mar 2025
-# Last updated : 22 Apr 2025
+# Last updated : 23 Apr 2025
 # Comments     :
 # TODO (Rick)  :
 # License      : GNU General Public License, version 2.0
@@ -27,9 +27,15 @@ fi
 
 set -eu
 
+## Global Variables ##
+
+readonly card_files=(cam-black.lst cam-white.lst)
+readonly card_dir="$HOME/bin/files"
+
+## Functions ##
+
 check_files() {
-  local file card_files
-  card_files=(cam-black.lst cam-white.lst)
+  local file
   check_package fzf
   for file in "${card_files[@]}"; do
     if [[ -f "$card_dir/$file" ]]; then
@@ -42,37 +48,41 @@ check_files() {
   done
 }
 
-main() {
-  local black_card white_card choices cards line script version card_dir black_file white_file
-  script=$(basename "$0")
-  version="1.4.25112"
-  card_dir="$HOME/.local/share/doc"
-  black_file="cam-black.lst"
-  white_file="cam-white.lst"
-  check_files
+play_game() {
+  local black_file white_file black_cards white_cards question_card answer_cards cards choices line ans
+  black_file="${card_files[0]}"
+  white_file="${card_files[1]}"
 
   # Create the arrays
   mapfile -t black_cards < "$card_dir/$black_file"
   mapfile -t white_cards < "$card_dir/$white_file"
 
   while true; do
-    clear
-    unset cards
-    black_card=$(printf "%s\n" "${black_cards[@]}" | shuf -n 1)
-    white_card=$(printf "%s\n" "${white_cards[@]}" | shuf -n 15)
+      clear
+      unset cards
+      question_card=$(printf "%s\n" "${black_cards[@]}" | shuf -n 1)
+      answer_cards=$(printf "%s\n" "${white_cards[@]}" | shuf -n 15)
 
-    IFS=$'\n'
-    while read -r line; do cards+=("$line"); done <<< "$white_card"
-    choices=("$(fzf --header="$black_card" --layout=reverse --border=bold --border=rounded --margin=5% --multi --prompt "Choose cards" < <(printf "%s\n" "${cards[@]}"))")
+      IFS=$'\n'
+      while read -r line; do cards+=("$line"); done <<< "$answer_cards"
+      choices=("$(fzf --header="$question_card" --layout=reverse --border=bold --border=rounded --margin=5% --multi --prompt "Choose cards" < <(printf "%s\n" "${cards[@]}"))")
 
-    box "Cards Against Muggles" "*"
-    printf "\n%s\n" "$black_card"
-    printf "%s\n\n" "${choices[@]}"
-    read -rsn1 -p "Press q to quit." ans
-    [[ "${ans,}" == "q" ]] && break
-  done
-  printf '\e[A\e[K'
-  printf "\nI hope you enjoyed playing Cards Against Muggles.\n"
+      box "Cards Against Muggles" "*"
+      printf "\n%s\n" "$question_card"
+      printf "%s\n\n" "${choices[@]}"
+      read -rsn1 -p "Press q to quit." ans
+      [[ "${ans,}" == "q" ]] && break
+    done
+    printf '\e[A\e[K'
+    printf "\nI hope you enjoyed playing Cards Against Muggles.\n"
+}
+
+main() {
+  local script version
+  script=$(basename "$0")
+  version="2.0.25113"
+  check_files
+  play_game
   over_line "$script $version"
   exit
 }
