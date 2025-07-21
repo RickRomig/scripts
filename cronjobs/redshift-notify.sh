3eee43acfbd295f7683ac68d6930097f7d99c1b8
Richard Romig <rick.romig@gmail.com>
2025-07-21 10:48:24 -0400

    cronjobs/redshift-notify.sh: added GPL statement

--- cronjobs/redshift-notify.sh
+++ cronjobs/redshift-notify.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-###############################################################################
+##########################################################################
 # Script Name  : redshift-notify.sh
 # Description  : send notification when Redshift changes between daytlight & night settings
 # Dependencies : xrandr
@@ -7,13 +7,24 @@
 # Author       : Copyright © 2024, Richard B. Romig, Mosfanet
 # Email        : rick.romig@gmail.com | rick.romig@mymetronet.com
 # Created      : 28 Oct 2024
-# Last updated : 29 May 2025 version 1.3.25149
+# Last updated : 21 Jul 2025 version 1.3.25202
 # Comments     : Script doesn't do anything if there is no display (on a KVM or headless)
 #              : Checks if Redshift has changed brightness.
 #              : Run as cron job: */10 * * * * /home/username/.local/bin/redshift-notify.sh
 # TODO (Rick)  :
 # License      : GNU General Public License, version 2.0
-###############################################################################
+# License URL  : https://github.com/RickRomig/scripts/blob/main/LICENSE
+##########################################################################
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+##########################################################################
 
 export DISPLAY=:0
 export DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/1000/bus"
