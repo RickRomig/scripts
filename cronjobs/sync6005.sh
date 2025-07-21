1cfaa79b5b96f842253932d950b2b0cda910db06
Richard Romig <rick.romig@gmail.com>
2025-07-21 10:55:07 -0400

    cronjobs/sync6005.sh: added GPL statement

--- cronjobs/sync6005.sh
+++ cronjobs/sync6005.sh
@@ -7,11 +7,22 @@
 # Author       : Copyright © 2023 Richard B. Romig, Mosfanet
 # Email        : rick.romig@gmail.com | rick.romig@mymetronet.net
 # Created      : 09 Dec 2023
-# Last updated : 19 Jun 2025 (version 1.1.24032)
-# Version      : 1.2.25170
+# Last updated : 21 Jul 2025
+# Version      : 1.2.25202
 # Comments     : Run in user crontab of main system
 # TODO (Rick)  :
 # License      : GNU General Public License, version 2.0
+# License URL  : https://github.com/RickRomig/scripts/blob/main/LICENSE
+##########################################################################
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
 ##########################################################################
 
 rsync -aq --delete "$HOME"/Downloads/archives/ rick@192.168.0.11:/data/archives/ 2>/dev/null
