40abba25164d2f844df58a5ea34561fe0762f7a0
Richard Romig <rick.romig@gmail.com>
2025-07-21 10:44:24 -0400

    cronjobs/hb-archive.sh: added GPL statement

--- cronjobs/hb-archive.sh
+++ cronjobs/hb-archive.sh
@@ -1,17 +1,28 @@
 #!/usr/bin/env bash
-###############################################################################
+##########################################################################
 # Script Name  : hb-archive.sh
 # Description  : Create HomeBank archive of *.bak files as a monthly cron job.
 # Dependencies : zip
 # Arguments    : none
 # Author       : Copyright (C) 2020, Richard B. Romig, 21 January 2020
 # Email        : rick.romig@gmail.com | rick.romig@mymetronet.net
-# Last updated : 08 Jul 2025
-# Version      : 4.3.25189
+# Last updated : 21 Jul 2025
+# Version      : 4.3.25200
 # Comments     : Run from user's crontab to run on the 1st of the month
 #              : to archive 2nd month previous. (1 May archives March files)
 # License      : GNU General Public License, version 2.0
-###############################################################################
+# License URL  : https://github.com/RickRomig/scripts/blob/main/LICENSE
+##########################################################################
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+##########################################################################
 
 set -eu
 
