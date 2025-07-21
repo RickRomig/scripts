055fc4e8cb59fddb2e175565abc00c49bba343ac
Richard Romig <rick.romig@gmail.com>
2025-07-21 10:46:27 -0400

    cronjobs/passwdsync.sh: added GPL statement

--- cronjobs/passwdsync.sh
+++ cronjobs/passwdsync.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-#############################################################################
+##########################################################################
 # Script Name  : passwdsync.sh
 # Description  : syncs password database with copy in Dropbox
 # Dependencies : none
@@ -7,12 +7,23 @@
 # Author       : Copyright (C) 2020, Richard B. Romig
 # Email        : rick.romig@gmail.com | rick.romig@mymetronet.net
 # Created      : 19 Aug 2020
-# Last updated : 08 Jul 2025
-# Version      : 4.16.25189
+# Last updated : 21 Jul 2025
+# Version      : 4.16.25202
 # Comments     : run as a local daily cron job
 # TODO (Rick)  :
 # License      : GNU General Public License, version 2.0
-#############################################################################
+# License URL  : https://github.com/RickRomig/scripts/blob/main/LICENSE
+##########################################################################
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+##########################################################################
 
 set -euo pipefail
 
