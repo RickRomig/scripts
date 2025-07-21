8a94a23d1aa120431812d6fccad6e2bbbad71f06
Richard Romig <rick.romig@gmail.com>
2025-07-21 10:50:03 -0400

    cronjobs/repo-backup.sh: added GPL statement

--- cronjobs/repo-backup.sh
+++ cronjobs/repo-backup.sh
@@ -7,10 +7,21 @@
 # Author       : Copyright © 2023 Richard B. Romig, Mosfanet
 # Email        : rick.romig@gmail.com | rick.romig@mymetronet.net
 # Created      : 16 Sep 2023
-# Last updated : 25 May 2024 Version 1.3.24146
+# Last updated : 21 Jul 2024 Version 1.3.25202
 # Comments     :
 # TODO (Rick)  :
 # License      : GNU General Public License, version 2.0
+# License URL  : https://github.com/RickRomig/scripts/blob/main/LICENSE
+##########################################################################
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
 ##########################################################################
 
 set -eu
