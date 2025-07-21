dbb5598c1c38b1b4a0da2dbe0142a935dbda3bf7
Richard Romig <rick.romig@gmail.com>
2025-07-21 14:16:17 -0400

    initial commit; scripts for in-place upgrade from Bookworm to Trixie

---
+++ 02-bookworm2trixie.sh
@@ -0,0 +1,103 @@
+#!/usr/bin/env bash
+##########################################################################
+# Script Name  : 02-bookworm2trixie.sh
+# Description  : Inplace upgrade from Debian 12 to Debian 13
+# Dependencies : apt-transport-https
+# Arguments    : none
+# Author       : Copyright © 2025 Richard B. Romig, Mosfanet
+# Email        : rick.romig@gmail.com | rick.romig@mymetronet.net
+# Created      : 06 Jun 2025
+# Last updated : 21 Jul 2025
+# Comments     : This scripts updates sources.list & backports.list.
+# TODO (Rick)  :
+# License      : GNU General Public License, version 2.0
+# License URL  : https://github.com/RickRomig/scripts/blob/main/LICENSE
+##########################################################################
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+##########################################################################
+
+## Shellcheck Directives ##
+# shellcheck source=/home/rick/bin/functionlib
+
+## Source function library ##
+
+if [[ -x "$HOME/bin/functionlib" ]]; then
+  source "$HOME/bin/functionlib"
+else
+  printf "\e[91mERROR:\e[0m functionlib not found!\n" >&2
+  exit 1
+fi
+
+set -eu
+
+## Functions ##
+
+check_files() {
+	[[ -f "$HOME/01-upgrade" ]] && return "$TRUE" || return "$FALSE"
+}
+
+version_info() {
+	printf "Version information:\n"
+	lsb_release -a | sed '1d'
+	printf "%-16s%s\n" "Version:" "$(cat /etc/debian_version)"
+}
+
+sources_list() {
+	# Backs up and updates sources list, leaving ftp url as http
+	local list_path="/etc/apt/"
+	local src_list="sources.list"
+	sudo_login 2
+	sudo sed -i.bak 's/http:/https:/;/ftp/s/https:/http:/;s/bookworm/trixie/' "$list_path/$src_list"
+}
+
+backports_list() {
+	# Backs up and updates backports list
+	local list_path="/etc/apt/sources.list.d"
+	local old_name="bookworm-backports.list"
+	local new_name="trixie-backports.list"
+	sudo_login 2
+	if [[ -f "$list_path/$old_name" ]]; then
+		sudo sed -i.bak 's/http:/https:/;s/bookworm/trixie/' "$list_path/$old_name"
+		sudo mv -v "$list_path/$old_name" "$list_path/$new_name"
+	else
+		printf "%s/%s does not exist.\n" "$list_path" "$old_name"
+	fi
+}
+
+upgrade_debian() {
+	# Cleans and updates apt cache, then upgrades to Debian 13.
+	sudo apt clean
+	sudo apt update
+	sudo apt upgrade --without-new-pkgs
+	sudo apt full-upgrade
+}
+
+main() {
+	local script="${0##*/}"
+	local version="1.2.25202"
+	local updated="21 Jul 2025"
+	check_files || die "01-bookworm2trixie.sh must be run first." 1
+	check_package apt-transport-https
+	version_info
+	[[ "$(lsb_release --codename --short | awk 'NR = 1 {print}')" == "bookworm" ]] || die "Unsupported Debian version." 1
+	printf "Updatings source lists...\n"
+	sources_list
+	backports_list
+	printf "Upgrading to Debian 13\n"
+	upgrade_debian
+	printf "Debian 13 Trixie installed.\n"
+	printf "Reboot and run 03-bookworm2trixie.sh\n"
+	touch "$HOME/02-sources"
+	over_line "$script $version ($updated)"
+}
+
+## Execution ##
+
+main "$@"
---
+++ 03-bookworm2trixie.sh
@@ -0,0 +1,82 @@
+#!/usr/bin/env bash
+##########################################################################
+# Script Name  : 03-bookworm2trixie.sh
+# Description  : Inplace upgrade from Debian 11 to Debian 12
+# Dependencies : none
+# Arguments    : none
+# Author       : Copyright © 2024 Richard B. Romig, Mosfanet
+# Email        : rick.romig@gmail.com | rick.romig@mymetronet.net
+# Created      : 06 Jun 2025
+# Last updated : 21 Jul 2025
+# Comments     : Final cleanup after upgrade to Debian 12.
+# TODO (Rick)  :
+# License      : GNU General Public License, version 2.0
+# License URL  : https://github.com/RickRomig/scripts/blob/main/LICENSE
+##########################################################################
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+##########################################################################
+
+## Shellcheck Directives ##
+# shellcheck source=/home/rick/bin/functionlib
+
+## Source function library ##
+
+if [[ -x "$HOME/bin/functionlib" ]]; then
+  source "$HOME/bin/functionlib"
+else
+  printf "\e[91mERROR:\e[0m functionlib not found!\n" >&2
+  exit 1
+fi
+
+# set -eu
+
+## Functions ##
+
+check_files() {
+	[[ -f "$HOME/01-upgrade" && -f "$HOME/02-sources" ]] && return "$TRUE" || return "$FALSE"
+}
+
+version_info() {
+	printf "Version information:\n"
+	lsb_release -a | sed '1d'
+	printf "%-16s%s\n" "Version:" "$(cat /etc/debian_version)"
+}
+
+clean_up() {
+	sudo_login 2
+	printf "Cleaning up after the upgrade...\n"
+	printf "\nCleaning the apt cache...\n"
+	sudo apt autoclean
+	printf "\nRemoving obsolete and unwanted programs...\n"
+	sudo apt --purge autoremove
+	printf "\nThere may be programs and applications that need to be reinstalled or updated.\n"
+	rm "$HOME/01-upgrade" "$HOME/02-sources"
+}
+
+main() {
+	local script="${0##*/}"
+	local version="1.1.25202"
+	local updated="21 Jul 2025"
+	check_files || die "01-bookworm2trixie.sh and 02-bookworm2trixie.sh must be run first." 1
+	version_info
+	if [[ "$(lsb_release --codename --short | awk 'NR = 1 {print}')" == "trixie" ]]; then
+		printf "Debian inplace upgrade was successful!\n"
+	else
+		printf "Debian inplace upgrade failed.\nBackup the home directory and any important files, then install from the ISO.\n" >&2
+	fi
+	clean_up
+	over_line "$script $version ($updated)"
+	exit
+}
+
+## Execution ##
+
+main "$@"
