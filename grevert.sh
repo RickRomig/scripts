9a2ced0c20ab26707436e39f9f752dff0a0b7127
Richard Romig <rick.romig@gmail.com>
2025-07-22 11:12:08 -0400

    grevert.sh: renamed and added GPL statement

--- grevert.sh
+++ grevert.sh
@@ -1,16 +1,27 @@
 #!/usr/bin/env bash
 ##########################################################################
-# Script Name  : grevert
+# Script Name  : grevert.sh
 # Description  : Revert most recent commit to the previous version after it's been pushed to repo
 # Dependencies : git
 # Arguments    : none
 # Author       : Copyright © 2024 Richard B. Romig, Mosfanet
 # Email        : rick.romig@gmail.com | rick.romig@mymetronet.net
 # Created      : 19 May 2024
-# Last updated : 07 Jul 2025
+# Last updated : 22 Jul 2025
 # Comments     : Commits that have been pushed to the repository.
 # TODO (Rick)  :
 # License      : GNU General Public License, version 2.0
+# License URL  : https://github.com/RickRomig/scripts/blob/main/LICENSE
+##########################################################################
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
 ##########################################################################
 
 ## Shellcheck Directives ##
@@ -40,7 +51,7 @@ git_revert() {
 
 main() {
   local script="${0##*/}"
-	local version="2.2.25188"
+	local version="2.2.25203"
 	check_package git
 	[[ "$(git rev-parse --is-inside-work-tree 2>/dev/null)" == "true" ]] || die "You are not in a git repositiory." 1
 	if y_or_n "Reset last commit (pushed) to the previous version?"; then
