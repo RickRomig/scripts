9b4521b80a97ef8ddb5392760eae9c3f5af2dc9d
Richard Romig <rick.romig@gmail.com>
2025-07-23 17:16:10 -0400

    snippets/check-digits.sh: added 3rd method for determining if argument is all digits

--- snippets/check-digits.sh
+++ snippets/check-digits.sh
@@ -9,6 +9,15 @@ method2() {
 	[[ "$1" =~ [^[:digit:]] ]] && echo "has non-digits" || echo "is all digits"
 }
 
-echo "By method 1, $1 $(method1 $1)"
-echo "BY method 2, $1 $(method2 $1)"
-exit
+method3() {
+	[[ "${1//[0-9]/}" ]] && echo "has non-digits" || echo "is all digits"
+}
+
+main() {
+	echo "By method 1, $1 $(method1 "$1")"
+	echo "BY method 2, $1 $(method2 "$1")"
+	echo "BY method 3, $1 $(method3 "$1")"
+	exit
+}
+
+main "$@"
