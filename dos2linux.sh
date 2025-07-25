90fa89ff39c96da477a733b1775f458361873f2f
Richard Romig <rick.romig@gmail.com>
2025-07-24 22:51:50 -0400

    dos2linux.sh: renamed; GPL statement

--- dos2linux.sh
+++ dos2linux.sh
@@ -1,16 +1,27 @@
 #!/usr/bin/env bash
 #####################################################################
-# Script Name  : dos2linux
+# Script Name  : dos2linux.sh
 # Description  : converts DOS text files to Linux format
 # Dependencies : none
 # Arguments    : file to be converted
 # Author       : Copyright (C) 2019, Richard Romig, 24 Jan 2019
 # Email        : rick.romig@gmail.com | rick.romig@mymetronet.net
 # Created      : 24 Jan 2019
-# Updated      : 30 Sep 2024
+# Updated      : 24 Jul 2025
 # TODO (Rick)  :
 # Comment      : removes DOS carriage return ('\r') characters
 # License      : GNU General Public License, version 2.0
+# License URL  : https://github.com/RickRomig/scripts/blob/main/LICENSE
+##########################################################################
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
 #####################################################################
 
 ## Shellcheck Directives ##
@@ -27,21 +38,19 @@ fi
 
 ## Global Variables ##
 
-script=$(basename "$0"); readonly script
-readonly version="1.2.24274"
+readonly script="${0##*/}"
+readonly version="1.2.25205"
 
 ## Functions ##
 
 help() {
-	local errcode updated
-	errcode="${1:-2}"
-	updated="30 Sep 2024"
+	local errcode="${1:-2}"
+	local updated="24 Jul 2025"
   cat << _HELP_
 ${orange}$script${normal} $version, Updated: $updated
 Utility to convert DOS text files to Linux format by removing carriage returns.
 
 ${green}Usage:${normal} $script [OPTION]
-
 ${orange}Available options:${normal}
   -h | --help  Show help
 NOTE: Converts files in the current working directory.
