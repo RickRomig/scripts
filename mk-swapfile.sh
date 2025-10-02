4a4d897d9cbee6661d33bffa3a320dd0c4d18f83
Richard Romig <rick.romig@gmail.com>
2025-10-01 23:02:48 -0400

    mk-swapfile.sh: create_swapfile() - added e4defrag to defrag swapfile

--- mk-swapfile.sh
+++ mk-swapfile.sh
@@ -3,11 +3,11 @@
 # Script Name  : mk-swapfile.sh
 # Description  : creates a swap file (/swapfile) in root directory
 # Dependencies : none
-# Arguments    : See help() function for available options.
+# Arguments    : none
 # Author       : Copyright © 2025 Richard B. Romig, Mosfanet
 # Email        : rick.romig@gmail.com | rick.romig@mymetronet.net
 # Created      : 27 Jan 2025
-# Last updated : 19 Jul 2025
+# Last updated : 01 Oct 2025
 # Comments     : creates swap file if no other swap exists.
 #              : Disable old swap and comment out in /etc/fstab
 # TODO (Rick)  :
@@ -37,8 +37,6 @@ else
   exit 1
 fi
 
-set -eu
-
 ## Functions ##
 
 append_fstab() {
@@ -56,6 +54,7 @@ create_swapfile() {
 	# create a 1GB swap file (multiply count by number of gigabyes needed)
 	sudo dd if=/dev/zero of=/swapfile bs=1024 count=1048576	# 4 x count = 4194304
 	ls -lh /swapfile  # see the file in the root directory
+	sudo e4defrag /swapfile	# defrag swapfile (1 contigous file)
 	sudo mkswap /swapfile # prepare swap file
 	sudo chmod 600 /swapfile  # set permissions
 	sudo swapon /swapfile # activate the swap file
@@ -64,7 +63,7 @@ create_swapfile() {
 
 main() {
 	local script="${0##*/}"
-	local version="1.1.25200"
+	local version="1.2.25274"
 	sudo_login 2
 	if swap_exists; then
 		printf "A swap device exists and is enabled.\n"
