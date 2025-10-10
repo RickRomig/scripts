72c36fb87f5b94a141594d83986594a16a272d6f
Richard Romig <rick.romig@gmail.com>
2025-10-09 20:02:56 -0400

    cronjobs/hb-archive.sh: moved path variables to archive_bak()

--- cronjobs/hb-archive.sh
+++ cronjobs/hb-archive.sh
@@ -6,8 +6,8 @@
 # Arguments    : none
 # Author       : Copyright (C) 2020, Richard B. Romig, 21 January 2020
 # Email        : rick.romig@gmail.com | rick.romig@mymetronet.net
-# Last updated : 03 Aug 2025
-# Version      : 4.3.25215
+# Last updated : 09 Oct 2025
+# Version      : 5.0.25282
 # Comments     : Run from user's crontab to run on the 1st of the month
 #              : to archive 2nd month previous. (1 May archives March files)
 # License      : GNU General Public License, version 2.0
@@ -24,10 +24,7 @@
 # GNU General Public License for more details.
 ##########################################################################
 
-set -eu
-
-# Functions
-
+# Trim top entry from the log file when the length exceeds 12 entries.
 trim_log() {
 	local log_len log_dir log_file
 	log_dir="$1"
@@ -36,24 +33,28 @@ trim_log() {
 	[[ "$log_len" -gt 12 ]] && sed -i '1d' "$log_dir/$log_file"
 }
 
+# Delete archive files older than 3 years.
 del_old_archives() {
 	local arc_dir="$1"
 	find "$arc_dir" -daystart -mtime +1095 -exec rm {} +
 }
 
+# Backup *.bak files from from 2 months previous.
 archive_bak() {
 	local arc_date arc_dir err_log hb_dir log_dir log_file ref_date status
-	arc_dir="$1"
-	log_dir="$2"
-	log_file="$3"
+	arc_dir=~/Downloads/archives/homebank
+	log_dir=~/.local/share/logs
+	log_file="HomeBank-archive.log"
 	arc_date=$(date -d "$(date +%Y-%m-01) - 2 months" +%Y-%m)
 	ref_date=$(date -d "$(date +%Y-%m-01) - 1 month" +%m%d%Y)
 	archive="$arc_date-backup.zip"
-	hb_dir="$HOME/Documents/HomeBank"
+	hb_dir=~/Documents/HomeBank
 	err_log="HomeBank-error.log"
 	status=0
-
-	{
+	# Create directories if they don't exist.
+	[[ -d "$log_dir" ]] || mkdir -p "$log_dir"
+	[[ -d "$arc_dir" ]] || mkdir -p "$arc_dir"
+{
 	  printf "%(%a|%F|%R)T|%s|" -1 "$arc_date"
 		if zip -qmtt "$ref_date" "$arc_dir/$archive" "$hb_dir"/*.bak 2> "$arc_dir/$err_log"; then
 	    printf "successful\n"
@@ -64,26 +65,13 @@ archive_bak() {
 	    printf "%(%F)T - HomeBank Archive had errors. (%s)" -1 "$status" >> "$arc_dir/$err_log"
 	  fi
 	} >> "$log_dir/$log_file"
+	trim_log "$log_dir" "$log_file"
+	del_old_archives "$arc_dir"
 }
 
 main() {
-	local arc_dir="$HOME/Downloads/archives/homebank"
-	local log_dir="$HOME/.local/share/logs"
-	local log_file="HomeBank-archive.log"
-
-	# Create directories if they don't exist.
-	[[ -d "$log_dir" ]] || mkdir -p "$log_dir"
-	[[ -d "$arc_dir" ]] || mkdir -p "$arc_dir"
-
-	# Backup *.bak files from from 2 months previous.
-	archive_bak "$arc_dir" "$log_dir" "$log_file"
-	# Trim top entry from the log file when the length exceeds 12 entries.
-	trim_log "$log_dir" "$log_file"
-	# Delete archive files older than 3 years.
-	del_old_archives "$arc_dir"
+	archive_bak
 	exit
 }
-
-# Execution
 
 main "$@"
