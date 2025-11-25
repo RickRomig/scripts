f891085955f869317ba4b1e5060543f09e6e3d5d
Richard Romig <rick.romig@gmail.com>
2025-11-25 13:07:13 -0500

    nosleep.sh: set_nosleep() - copies sleep.conf to sleep.conf.d and configures with sed file

--- nosleep.sh
+++ nosleep.sh
@@ -7,7 +7,7 @@
 # Author       : Copyright © 2023 Richard B. Romig, Mosfanet
 # Email        : rick.romig@gmail.com | rick.romig@mymetronet.net
 # Created      : 18 Jul 2023
-# Last updated : 19 Jul 2025
+# Last updated : 25 Jul 2025
 # Comments     :
 # TODO (Rick)  :
 # License      : GNU General Public License, version 2.0
@@ -36,30 +36,31 @@ else
   exit 1
 fi
 
-set -eu
-
 ## Functions ##
 
 set_nosleep() {
   local script_dir
   script_dir=$(dirname "$(readlink -f "${0}")")
-  local -r sleep_file="/etc/systemd/sleep.conf"
+  local -r sleep_dir="/etc/systemd/sleep.conf.d"
+  local -r sysd_dir="/etc/systemd"
+  local -r sleep_file="sleep.conf"
   local -r sed_file="$script_dir/files/nosleep.sed"
-  if [[ -f "$sed_file" ]]; then
-    sudo_login 2
-    sudo sed -i.bak -f "$sed_file" "$sleep_file"
-    printf "%s modified. Backup (%s.bak) created.\n" "$sleep_file" "${sleep_file##*/}"
-    return 0
-  else
-    printf "A required file (%s) was not found.\n" "${sed_file##*/}"  >&2
+  if [[ ! -f "$sed_file" ]]; then
+    printf "A required file (%s) was not found.\n" "${sed_file##*/}" >&2
     printf "Operation could not continue.\n" >&2
     return 1
   fi
+  sudo_login 2
+  [[ -d "$sleep_dir" ]] || sudo mkdir -p "$sleep_dir"
+  sudo cp -v "$sysd_dir/$sleep_file" "$sleep_dir/"
+  sudo sed -i.bak -f "$sed_file" "$sleep_dir/$sleep_file"
+  printf "%s modified. Backup (%s.bak) created.\n" "$sleep_file" "${sleep_file##*/}"
+  return 0
 }
 
 main() {
   local script="${0##*/}"
-  local -r version="1.4.25200"
+  local -r version="2.0.25329"
   printf "Disables sleep and hiberation on Debian-based systems.\n"
   set_nosleep; code="$?"
   over_line "$script $version"
