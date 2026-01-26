4d406921d8f67cf0e2ee3cf5b0ba1988b2f88e09
Richard Romig <rick.romig@gmail.com>
2026-01-26 18:10:04 -0500

    grevert.sh: git_revert() - replaced pipes to head and cut with process subsitution to awk

--- grevert.sh
+++ grevert.sh
@@ -7,7 +7,7 @@
 # Author       : Copyright © 2024 Richard B. Romig, Mosfanet
 # Email        : rick.romig@gmail.com | rick.romig@mymetronet.net
 # Created      : 19 May 2024
-# Last updated : 22 Jul 2025
+# Last updated : 26 Jan 2026
 # Comments     : Commits that have been pushed to the repository.
 # TODO (Rick)  :
 # License      : GNU General Public License, version 2.0
@@ -33,25 +33,29 @@ if [[ -x "$HOME/bin/functionlib" ]]; then
   source "$HOME/bin/functionlib"
 else
   printf "\e[91mERROR:\e[0m functionlib not found!\n" >&2
-  exit 1
+  exit 81
 fi
+
+## Global Variables ##
+
+readonly E_NOT_GIT_REPO=92
 
 ## Functions ##
 
 git_revert() {
 	local hash
-	hash=$(git log --oneline | head -n 1 | cut -d' ' -f1)
-	printf "Reverting the most recent pushed commit to the previous version.\n"
+	hash-"$(awk 'NR==1 {print $1}' <(git log --oneline))"
+	printf "Reverting the most recent pushed commit (%s) to the previous version.\n" "$hash"
 	git revert "$hash" --no-edit
-	git log --oneline | head -n 2
+	head -n 2 <(git log --oneline)
 	git push
 }
 
 main() {
   local script="${0##*/}"
-	local version="2.2.25203"
+	local version="2.3.26026"
 	check_package git
-	[[ "$(git rev-parse --is-inside-work-tree 2>/dev/null)" == "true" ]] || die "You are not in a git repositiory." 1
+	[[ "$(git rev-parse --is-inside-work-tree 2>/dev/null)" == "true" ]] || die "You are not in a git repositiory."  "$E_NOT_GIT_REPO"
 	if y_or_n "Reset last commit (pushed) to the previous version?"; then
 		git_revert
 	else
