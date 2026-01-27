83dffbf49f2c085bd37743e3cd3cc63416daef6c
Richard Romig <rick.romig@gmail.com>
2026-01-27 17:56:33 -0500

    retire-script.sh: refactored - added check_args(); modified check_git_repo()

--- retire-script.sh
+++ retire-script.sh
@@ -7,7 +7,7 @@
 # Author       : Copyright © 2024 Richard B. Romig, LudditeGeek@Mosfanet
 # Email        : rick.romig@gmail.com | rick.romig@mymetronet.net
 # Created      : 04 Jul 2024
-# Last updated : 11 Oct 2025
+# Last updated : 27 Jan 202
 # Comments     : Do not use with scripts or files inside git repos. Use gretire.sh instead.
 # TODO (Rick)  :
 # License      : GNU General Public License, version 2.0
@@ -39,13 +39,15 @@ fi
 ## Global Variables ##
 
 readonly script="${0##*/}"
-readonly version="3.0.252284"
+readonly version="3.1.26027"
+readonly E_NOT_GIT_REPO=92
+EC=0
 
 ## Functions ##
 
 help() {
-	local errcode="${1:-2}"
-	local updated=" 11 Oct 2025"
+	local errcode="${1:-1}"
+	local updated=" 27 Jan 202"
 	cat << _HELP_
 ${orange}$script${normal} $version ($updated)
 Retires a script by moving it to a zipped archive.
@@ -54,10 +56,16 @@ ${green}Usage:${normal} $script <script-name>
 ${orange}Available options:${normal}
   -h | --help  Show this help message and exit
 ${bold}NOTES:${normal}
-1. Do not use to retire a script in a git repository.
-2. To properly retire a script in a git repository use 'gretire.sh'.
+1. User is prompted to provide a file mame is no arguments are supplied.
+2. Do not use to retire a script in a git repository.
+3. To properly retire a script in a git repository use 'gretire.sh'.
 _HELP_
 	exit "$errcode"
+}
+
+exit_script() {
+  over_line "$script $version"
+	exit "$EC"
 }
 
 check_dependencies() {
@@ -69,16 +77,25 @@ check_git_repo() {
   if [[ "$(git rev-parse --is-inside-work-tree 2>/dev/null)" == "true" ]]; then
 		printf "%s This is a git repository.\n" "$RED_WARNING" >&2
 		printf "Use 'gretire.sh' to retire a script inside a git repository\n" >&2
-		over_line "$script $version"
-		exit
+		EC="$E_NOT_GIT_REPO"
+		exit_script
+	fi
+}
+
+chack_args() {
+	local filename="$1"
+	if [[ ! -f "$filename" ]]; then
+		printf "%s was not found in this directory.\n" "$filename" >&2
+		EC="$E_FILENOTFOUND"
+		exit_script
 	fi
 }
 
 retire_script() {
-	local filename retname archive_dir ret_archive
-	filename="$1"
+	local filename archive_dir ret_archive retname
 	archive_dir="$HOME/Downloads/archives"
 	ret_archive="retired-scripts.zip"
+	filename="$1"
 	retname="${filename}.$(date +'%y%j')"
 	mv -v "$filename" "$retname"
 	zip -um "$archive_dir/$ret_archive$" "$retname"
@@ -88,13 +105,12 @@ retire_script() {
 main() {
 	[[ "$1" == "-h" || "$1" == "--help" ]] && help 0
 	local filename="$1"
-	check_git_repo
-	[[ "$#" -eq 0 ]] && read -rp "Enter a script to retire: " filename
-	[[ -f "$filename" ]] || { printf "%s %s not found.\n" "$RED_ERROR" "$filename" >&2; help 2; }
+	[[ "$filename" ]] || read -rp "Enter a script to be retired: " filename
 	check_dependencies
+	check_git_repo
+	chack_args "$filename"
 	retire_script "$filename"
-  over_line "$script $version"
-	exit
+	exit_script
 }
 
 ## Execution ##
