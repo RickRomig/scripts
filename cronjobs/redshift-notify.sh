695de4515229f53892489a68193b0f96e3721fe8
Richard Romig <rick.romig@gmail.com>
2026-05-03 19:29:22 -0400

    cronjobs/redshift-notify.sh: employed process substiution and parameter expansion to set brightness variable

--- cronjobs/redshift-notify.sh
+++ cronjobs/redshift-notify.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-##########################################################################
+###############################################################################
 # Script Name  : redshift-notify.sh
 # Description  : send notification when Redshift changes between daytlight & night settings
 # Dependencies : xrandr
@@ -7,14 +7,15 @@
 # Author       : Copyright © 2024, Richard B. Romig, Mosfanet
 # Email        : rick.romig@gmail.com | rick.romig@mymetronet.com
 # Created      : 28 Oct 2024
-# Last updated : 21 Jul 2025 version 1.3.25202
+# Last updated : 03 May 2026
+# Version      : 1.4.26123
 # Comments     : Script doesn't do anything if there is no display (on a KVM or headless)
 #              : Checks if Redshift has changed brightness.
 #              : Run as cron job: */10 * * * * /home/username/.local/bin/redshift-notify.sh
 # TODO (Rick)  :
 # License      : GNU General Public License, version 2.0
 # License URL  : https://github.com/RickRomig/scripts/blob/main/LICENSE
-##########################################################################
+###############################################################################
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 2 of the License, or
@@ -24,7 +25,7 @@
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 # GNU General Public License for more details.
-##########################################################################
+###############################################################################
 
 export DISPLAY=:0
 export DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/1000/bus"
@@ -32,7 +33,9 @@ export DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/1000/bus"
 DAY_FILE=/tmp/daytime
 NIGHT_FILE=/tmp/nighttime
 
-brightness=$(xrandr --verbose | grep -w Brightness -m1 | cut -d' ' -f2 )
+# brightness=$(xrandr --verbose | grep -w Brightness -m1 | cut -d' ' -f2 )
+brightness=$(grep -w Brightness -m1 < <(xrandr --verbose))
+brightness="${brightness##* }"
 
 if [[ "$brightness" == "1.0" && -f "$NIGHT_FILE" ]]; then
 	rm "$NIGHT_FILE"
