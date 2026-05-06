7d920305808c2353b6c98a31dfd3586a0a434dbd
Richard Romig <rick.romig@gmail.com>
2026-05-05 21:54:29 -0400

    grevert.sh: changed sourcing of functionlib;main() added E_NOT_GIT_REPO local variable

--- grevert.sh
+++ grevert.sh
@@ -7,7 +7,7 @@
 # Author       : Copyright © 2024 Richard B. Romig, Mosfanet
 # Email        : rick.romig@gmail.com | rick.romig@mymetronet.net
 # Created      : 19 May 2024
-# Last updated : 26 Jan 2026
+# Last updated : 05 May 2026
 # Comments     : Commits that have been pushed to the repository.
 # TODO (Rick)  :
 # License      : GNU General Public License, version 2.0
@@ -24,23 +24,15 @@
 # GNU General Public License for more details.
 ##########################################################################
 
-## Shellcheck Directives ##
-# shellcheck source=/home/rick/bin/functionlib
-
 ## Source function library ##
-
-if [[ -x "$HOME/bin/functionlib" ]]; then
-  source "$HOME/bin/functionlib"
-else
-  printf "\e[91mERROR:\e[0m functionlib not found!\n" >&2
-  exit 81
-fi
+# shellcheck source=/home/rick/bin/functionlib
+source functionlib || { printf "\e[91mERROR:\e[0m Unable to source functionlib\n"; exit 1; }
 
 ## Functions ##
 
 git_revert() {
 	local hash
-	hash="$(awk 'NR==1 {print $1}' <(git log --oneline))"
+	hash="$(awk 'NR==1 {print $1}' < <(git log --oneline))"
 	printf "Reverting the most recent pushed commit (%s) to the previous version.\n" "$hash"
 	git revert "$hash" --no-edit
 	head -n 2 <(git log --oneline)
@@ -48,10 +40,11 @@ git_revert() {
 }
 
 main() {
-  local script="${0##*/}"
-	local version="2.3.26026"
+  local -r script="${0##*/}"
+	local -r version="2.4.26125"
+	local -r E_NOT_GIT_REPO=101
 	check_package git
-	[[ "$(git rev-parse --is-inside-work-tree 2>/dev/null)" == "true" ]] || die "You are not in a git repositiory."  1
+	[[ "$(git rev-parse --is-inside-work-tree 2>/dev/null)" == "true" ]] || die "You are not in a git repositiory." "$E_NOT_GIT_REPO"
 	if y_or_n "Reset last commit (pushed) to the previous version?"; then
 		git_revert
 	else
