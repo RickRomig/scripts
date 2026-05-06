d9eee313704c10467eceb8577c74f227d72107eb
Richard Romig <rick.romig@gmail.com>
2026-05-05 21:53:39 -0400

    greset.sh: changed sourcing of functionlib;main() added E_NOT_GIT_REPO local variable

--- greset.sh
+++ greset.sh
@@ -7,7 +7,7 @@
 # Author       : Copyright © 2024 Richard B. Romig, Mosfanet
 # Email        : rick.romig@gmail.com | rick.romig@mymetronet.net
 # Created      : 19 May 2024
-# Last updated : 01 Mar 2026
+# Last updated : 05 May 2026
 # Comments     : Commits that haven't been pushed to the repository.
 # TODO (Rick)  :
 # License      : GNU General Public License, version 2.0
@@ -24,33 +24,26 @@
 # GNU General Public License for more details.
 ##########################################################################
 
-## Shellcheck Directives ##
-# shellcheck source=/home/rick/bin/functionlib
-
 ## Source function library ##
-
-if [[ -x "$HOME/bin/functionlib" ]]; then
-  source "$HOME/bin/functionlib"
-else
-  printf "\e[91mERROR:\e[0m functionlib not found!\n" >&2
-  exit 81
-fi
+# shellcheck source=/home/rick/bin/functionlib
+source functionlib || { printf "\e[91mERROR:\e[0m Unable to source functionlib\n"; exit 1; }
 
 ## Functions ##
 
 git_reset() {
 	local hash
-	hash="$(awk 'NR==1 {print $1}' <(git log --oneline))"
+	hash="$(awk 'NR==1 {print $1}' < <(git log --oneline))"
 	printf "Resetting the most recent commit (%s) that has not been pushed to the remote repository.\n" "$hash"
 	git reset "$hash"
 	head -n 2 <(git log --oneline)
 }
 
 main() {
-  local script="${0##*/}"
-	local version="2.5.26060"
+  local -r script="${0##*/}"
+	local -r version="2.5.26060"
+	local -r E_NOT_GIT_REPO=101
 	check_package git
-	[[ "$(git rev-parse --is-inside-work-tree 2>/dev/null)" == "true" ]] || die "You are not in a git repositiory." 1
+	[[ "$(git rev-parse --is-inside-work-tree 2>/dev/null)" == "true" ]] || die "You are not in a git repositiory." "$E_NOT_GIT_REPO"
 	if y_or_n "Reset last unpushed commit to the previous version?"; then
 		git_reset
 	else
