1e49b770db406887610baa14be8c29a6886cf466
Richard Romig <rick.romig@gmail.com>
2026-05-13 11:28:35 -0400

    binary-i386.sh: refactored, adding check_i386_arch() and check_amd64_arch()

--- binary-i386.sh
+++ binary-i386.sh
@@ -7,7 +7,7 @@
 # Author       : Copyright © 2026, Richard B. Romig, Mosfanet
 # Email        : rick.romig@gmail | rick.romig@mymetronet.net
 # Created      : 15 Apr 2026
-# Last updated : 15 Apr 2026
+# Last updated : 13 May2026
 # Comments     : Fixes main/binary-i386 error message for Google Chrome update
 # TODO (Rick)  :
 # License      : GNU General Public License, version 2.0
@@ -26,7 +26,7 @@
 
 ## Source function library ##
 # shellcheck source=/home/rick/bin/functionlib
-source functionlib || { printf "\e[91mERROR:\e[0m Unable to source functionlib\n"; exit 1; }
+source ~/bin/functionlib || { printf "\e[91mERROR:\e[0m Unable to source functionlib\n"; exit 1; }
 
 ## Functions ##
 
@@ -37,35 +37,47 @@ echo "\"https://dl.google.com/linux/chrome-stable/deb stable InRelease\""
 echo "doesn't support architecture 'i386'"
 }
 
-remove_i386_arch() {
-	local foreign_arch native_arch num_386
-	printf "\nConfirm that 64-bit with multiarch is enabled.\n"
+check_i386_arch() {
+	local foreign_arch
 	foreign_arch=$(dpkg --print-foreign-architectures)
-	if [[ $foreign_arch == "i386" ]]; then
-		printf "32-bit support has been added.\n"
+	[[ $foreign_arch == "i386" ]] && return "$TRUE" || return "$FALSE"
+}
+
+check_amd64_arch() {
+	local  native_arch
+	native_arch=$(dpkg --print-architecture)
+	printf "%+4sNative architecture is %s.\n" "" "$native_arch"
+	[[ $native_arch == "amd64" ]] && return "$TRUE" || return "$FALSE"
+}
+
+remove_i386_arch() {
+	local  native_arch num_386
+	printf "\nConfirm that 64-bit with multiarch is enabled...\n"
+	if check_i386_arch; then
+		printf "%+4s32-bit support has been added.\n" ""
 	else
-		printf "32-bit support has not been added.\n"
-		return
+		printf "%+4s32-bit support has not been added.\n" ""
 	fi
 	printf "Confirm native architecture...\n"
-	native_arch=$(dpkg --print-architecture)
-	printf "Native architecture is %s.\n" "$native_arch"
-	[[ $native_arch != "amd64" ]] && return
+	check_amd64_arch || return
 	num_386=$(grep -c 386 < <(dpkg --get-selections))
-	printf "Number of 32-bit packages = %d\n" "$num_386"
-	if [[ $num_386 -eq 0 ]]; then
-		printf "Removing support for i386 architecture...\n"
-		# grep i386 < <(sudo dpkg --remove-architecture i386)
-		printf "i386 support removed.\n"
-	else
-		printf "32-bit applications are present.\n"
-		dpkg --get-selections | grep 386
+	if [[ $num_386 -gt 0 ]]; then
+		printf "Number of 32-bit packages = %d\n" "$num_386"
+		printf "32-bit applicaitons:\n"
+		grep 386 < <(dpkg --get-selections)
+		return
 	fi
+	check_i386_arch || return
+	sudo_login 2
+	printf "%+4sRemoving i386 architecture support...\n" ""
+	grep i386 < <(sudo dpkg --remove-architecture i386)
+	echo "${PIPESTATUS[@]}"
+	printf "i386 support removed.\n"
 }
 
 main() {
   local -r script="${0##*/}"
-  local -r version="1.0.26105"
+  local -r version="2.0.26133"
   intro_message
   remove_i386_arch
   over_line "$script $version"
