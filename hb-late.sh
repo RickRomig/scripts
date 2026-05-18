4d854a33c8ede40de3adb9fe95c38123a182f790
Richard Romig <rick.romig@gmail.com>
2026-05-18 10:30:32 -0400

    hb-late.sh: updated function and variable names

--- hb-late.sh
+++ hb-late.sh
@@ -8,7 +8,7 @@
 # Email        : rick.romig@gmail.com | rick.romig@mymetronet.net
 # Comments     : Use only if hb-archive.sh fails to run on 1st of the month.
 # Created      : 02 Sep 2019
-# Updatd       : 20 Mar 2026
+# Updatd       : 18 May 2026
 # TODO (rick)  :
 # License      : GNU General Public License, version 2.0
 # License URL  : https://github.com/RickRomig/scripts/blob/main/LICENSE
@@ -24,29 +24,21 @@
 # GNU General Public License for more details.
 ###############################################################################
 
-## Shellcheck Directives ##
-# shellcheck source=/home/rick/bin/functionlib
-
 ## Load function library ##
-
-if [[ -x "$HOME/bin/functionlib" ]]; then
-  source "$HOME/bin/functionlib"
-else
-  echo -e "\e[91mERROR:\e[0m functionlib not found!" >&2
-  exit 81
-fi
+# shellcheck source=/home/rick/bin/functionlib
+source ~/bin/functionlib || { printf "\e[91mERROR:\e[0m Unable to source functionlib\n"; exit 1; }
 
 ## Functions ##
 
 trim_log() {
-	local log_len log_dir log_file
-	log_dir="$1"
-	log_file="$2"
+	local -i log_len
+	local -r log_dir="$1"
+	local -r log_file="$2"
 	log_len=$(wc -l < "$log_dir/$log_file")
-	[[ "$log_len" -gt 12 ]] && sed -i '1d' "$log_dir/$log_file"
+  (( log_len > 12 )) && sed -i '1d' "$log_dir/$log_file"
 }
 
-del_old() {
+del_old_archives() {
 	local arc_dir="$1"
 	find "$arc_dir" -daystart -mtime +1095 -exec rm {} +
 }
@@ -55,8 +47,8 @@ update_log_files() {
   local status="$1"
   local arc_date="$2"
   local err_log="$3"
-  local arc_dir="$HOME/Downloads/archive/homebank"
-  local log_dir="$HOME/.local/share/logs"
+  local arc_dir=~/Downloads/archive/homebank
+  local log_dir=~/.local/share/logs
   local log_file="HomeBank-archive.log"
   # Write to log in the archive directory
   echo "${orange}HomeBank backup files for $arc_date archived.${normal}"
@@ -72,14 +64,14 @@ update_log_files() {
     (( status = 0 )) && printf "successful\n" || printf "had errors\n"
   } >> "$log_dir/$log_file"
 	trim_log "$log_dir" "$log_file"
-	del_old "$arc_dir"
- }
+	del_old_archives "$arc_dir"
+}
 
 monthly_archive() {
-  local ref_date arc_date arc_dir hb_archive hb_dir status
-  arc_dir=$HOME"/Downloads/archive/homebank"
-  err_log="HomeBank-error.log"
-  hb_dir=$HOME"/Documents/HomeBank"
+  local ref_date arc_date hb_archive status
+  local -r arc_dir=~/Downloads/archive/homebank
+  local -r err_log="HomeBank-error.log"
+  local -r hb_dir=~/Documents/HomeBank
   hb_archive=$(date -d "$(date +%Y-%m-01) - 2 months" +%Y-%m)-backup.zip
   arc_date=$(date -d "$(date +%Y-%m-01) - 2 months" +"%B %Y")  # 2 calendar months previous
   ref_date=$(date -d "$(date +%Y-%m-01) - 1 month" +%m%d%Y) # first day of the previous month
@@ -90,16 +82,16 @@ monthly_archive() {
 }
 
 main() {
-  local script="${0##*/}"
-  local version="4.0.26079"
-  local lhost="${HOSTNAME:-$(hostname)}"
-  local fhost="hp-800g2-sff"
+  local -r script="${0##*/}"
+  local -r version="4.1.26138"
+  local -r lhost="${HOSTNAME:-$(hostname)}"
+  local -f fhost="hp-800g2-sff"
 
-  [[ "$lhost" != "$fhost" ]] && die "$script must be run from the $fhost."
+  [[ "$lhost" != "$fhost" ]] && die "$script must be run from the $fhost." "$E_INVALID_HOST"
   check_package zip
   printf "Archives .bak files if hb-archive.sh doesn't run on the 1st of the month.\n"
   monthly_archive
-  over_line "$script $version "
+  over_line "$script $version"
   exit
 }
 
