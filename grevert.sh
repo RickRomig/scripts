7cf9e4b7f8fb388a8bfa6c9b250406ca28b18c43
Richard Romig <rick.romig@gmail.com>
2026-05-18 10:04:57 -0400

    grevert.sh: addedis_git_repo() to check if in a git repo

--- grevert.sh
+++ grevert.sh
@@ -7,7 +7,7 @@
 # Author       : Copyright © 2024 Richard B. Romig, Mosfanet
 # Email        : rick.romig@gmail.com | rick.romig@mymetronet.net
 # Created      : 19 May 2024
-# Last updated : 05 May 2026
+# Last updated : 18 May 2026
 # Comments     : Commits that have been pushed to the repository.
 # TODO (Rick)  :
 # License      : GNU General Public License, version 2.0
@@ -26,29 +26,33 @@
 
 ## Source function library ##
 # shellcheck source=/home/rick/bin/functionlib
-source functionlib || { printf "\e[91mERROR:\e[0m Unable to source functionlib\n"; exit 1; }
+source ~/bin/functionlib || { printf "\e[91mERROR:\e[0m Unable to source functionlib\n"; exit 1; }
 
 ## Functions ##
+
+is_git_repo() {
+  [[ "$(git rev-parse --is-inside-work-tree 2>/dev/null)" == "true" ]]  && return "$TRUE" || return "$FALSE"
+}
 
 git_revert() {
 	local hash
 	hash="$(awk 'NR==1 {print $1}' < <(git log --oneline))"
 	printf "Reverting the most recent pushed commit (%s) to the previous version.\n" "$hash"
 	git revert "$hash" --no-edit
-	head -n 2 <(git log --oneline)
+	head -n 2 < <(git log --oneline)
 	git push
 }
 
 main() {
   local -r script="${0##*/}"
-	local -r version="2.4.26125"
+	local -r version="2.5.26138"
 	local -r E_NOT_GIT_REPO=101
 	check_package git
-	[[ "$(git rev-parse --is-inside-work-tree 2>/dev/null)" == "true" ]] || die "You are not in a git repositiory." "$E_NOT_GIT_REPO"
-	if y_or_n "Reset last commit (pushed) to the previous version?"; then
+	is_git_repo || die "You are not in a git repositiory." "$E_NOT_GIT_REPO"
+	if y_or_n "Reset last pushed commit to the previous version?"; then
 		git_revert
 	else
-		printf "Last commit unchanged.\n"
+		printf "Last pushed commit unchanged.\n"
 	fi
 	over_line "$script $version"
 	exit
