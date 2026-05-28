7bbe1bd6de69adb8c40b5756e3505f29f540aa8d
Richard Romig <rick.romig@gmail.com>
2026-05-28 09:38:47 -0400

    no-flatpak.sh: pref_file & script_dir local variables; enable_flatpak() & disable_flatpak() return if no pref_file

--- no-flatpak.sh
+++ no-flatpak.sh
@@ -7,7 +7,7 @@
 # Author       : Copyright (C) 2024, Richard B. Romig, MosfaNet
 # Email        : rick.romig@gmail.com | rick.romig@mymetronet.net
 # Created      : 02 Mar 2024
-# Updated      : 05 Feb 2026
+# Updated      : 28 May 2026
 # Comments     :
 # TODO (Rick)  :
 # License      : GNU General Public License, version 2.0
@@ -24,30 +24,20 @@
 # GNU General Public License for more details.
 ##########################################################################
 
-## Shellcheck Directives ##
+## Load function library ##
 # shellcheck source=/home/rick/bin/functionlib
-
-# Load function library ##
-
-if [[ -x "$HOME/bin/functionlib" ]]; then
-  source "$HOME/bin/functionlib"
-else
-  printf "\e[91mERROR:\e[0m functionlib not found!\n" >&2
-  exit 81
-fi
+source ~/bin/functionlib || { printf "\e[91mERROR:\e[0m Unable to source functionlib\n"; exit 1; }
 
 ## Global Variables ##
 
 readonly script="${0##*/}"
-readonly version="1.4.26036"
-readonly pref_file="/etc/apt/preferences.d/noflatpak.pref"
-script_dir=$(dirname "$(readlink -f "${0}")"); readonly script_dir
+readonly version="1.5.26148"
 
 ## Functions ##
 
 help() {
 	local errcode="${1:-1}"
-	local updated="05 Feb 2026"
+	local updated="28 May 2026"
 	cat << _HELP_
 ${orange}$script${normal} $version, Updated $updated
 Disables/Enables Flatpak support.
@@ -63,10 +53,11 @@ _HELP_
 }
 
 flatpak_installed() {
-	 exists flatpak && return "$TRUE" || return "$FALSE"
+	 installed flatpak && return "$TRUE" || return "$FALSE"
 }
 
 flatpak_enabled() {
+	local -r pref_file="$1"
 	if [[ -f "$pref_file" ]]; then
 		grep -q '^# Package:' "$pref_file" && return "$TRUE" || return "$FALSE"
 	else
@@ -75,32 +66,35 @@ flatpak_enabled() {
 }
 
 enable_flatpak() {
-	if [[ -f "$pref_file" ]]; then
-		if grep -q '^Package:' "$pref_file"; then
-			sudo_login 1
-			sudo sed -i '/^Package/s/^/# /;/^Pin/s/^/# /' "$pref_file"
-			printf "Installation of Flatpak and Flatpak packages is now enabled.\n"
-		else
-      printf "Installation of Flatpak and Flatpak packages is already enabled by %s.\n" "$pref_file"
-    fi
-  else
+	local -r pref_file="$1"
+	if [[ ! -f "$pref_file" ]]; then
     printf "%s does not exist.\nInstallation of Flatpak and Flatpak packages is enabled by default.\n" "$pref_file"
+		return
+  fi
+	if grep -q '^Package:' "$pref_file"; then
+		sudo_login 1
+		sudo sed -i '/^Package/s/^/# /;/^Pin/s/^/# /' "$pref_file"
+		printf "Installation of Flatpak and Flatpak packages is now enabled.\n"
+	else
+    printf "Installation of Flatpak and Flatpak packages is already enabled by %s.\n" "$pref_file"
   fi
 }
 
 disable_flatpak() {
-  if [[ -f "$pref_file" ]]; then
-    if grep -q '^# Package:' "$pref_file"; then
-			sudo_login 1
-      sudo sed -i '/Package/s/^# //;/Pin/s/^# //' "$pref_file"
-      printf "\nInstallation of Flatpak and Flatpak packages is now disabled.\n"
-    else
-      printf "\nInstallation of Flatpak and Flatpak packages is already disabled.\n"
-    fi
-  else
+	local -r pref_file="$1"
+	local -r script_dir=$(dirname "$(readlink -f "${0}")")
+  if [[ ! -f "$pref_file" ]]; then
 		sudo_login 1
     sudo cp "$script_dir/files/${pref_file##*/}" "${pref_file%/*}/"
     printf "%s has been created. Installation of Flatpak and Flatpak packages is now disabled.\n" "$pref_file"
+		return
+  fi
+  if grep -q '^# Package:' "$pref_file"; then
+		sudo_login 1
+    sudo sed -i '/Package/s/^# //;/Pin/s/^# //' "$pref_file"
+    printf "\nInstallation of Flatpak and Flatpak packages is now disabled.\n"
+  else
+    printf "\nInstallation of Flatpak and Flatpak packages is already disabled.\n"
   fi
 }
 
@@ -108,22 +102,23 @@ main() {
   local noOpt opt optstr OPTARG OPTIND
 	printf "Flatpack is "
 	flatpak_installed && printf "installed.\n" || printf "not installed.\n"
+	local -r pref_file="/etc/apt/preferences.d/noflatpak.pref"
 	noOpt=1
 	optstr=":dehs"
 	while getopts "$optstr" opt; do
 		case "$opt" in
 			d )
-				disable_flatpak
+				disable_flatpak "$pref_file"
 				;;
 			e )
-				enable_flatpak
+				enable_flatpak "$pref_file"
 				;;
 			h )
 				help 0
 				;;
 			s )
 				printf "Installation of Flatpak and Flatpak packages is "
-				flatpak_enabled && printf "enabled.\n" || printf "disabled.\n"
+				flatpak_enabled "$pref_file" && printf "enabled.\n" || printf "disabled.\n"
 				;;
 			? )
 				printf "\n%s Invalid option -%s\n" "$RED_ERROR" "$OPTARG" >&2
