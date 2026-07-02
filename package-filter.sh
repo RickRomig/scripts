96245c00f4ee653f918aeaf365ee8505c820a623
Richard Romig <rick.romig@gmail.com>
2026-07-01 21:15:23 -0400

    package-filter.sh: search_packages() - replaced read prompt with default_no(); checks if package already installed

--- package-filter.sh
+++ package-filter.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-###############################################################################
+#####################################################################################
 # Script Name  : package-filter.sh
 # Description  : Filters available packages with FZF
 # Dependencies : fzf
@@ -7,13 +7,13 @@
 # Author       : Copyright © 2025, Richard B. Romig, Mosfanet
 # Email        : rick.romig@gmail.com | rick.romig@mymetronet.com
 # Created      : 15 Oct 2025
-# Last updated : 07 Apr 2026
+# Last updated : 01 Jul 2026
 # Comments     : Original script Copyright (C) 2025  Kris Occhipinti
 #              : Modified and adapted by Rick Romig
 # TODO (Rick)  :
 # License      : GNU General Public License, version 2
 # License URL  : https://github.com/RickRomig/scripts/blob/main/LICENSE
-###############################################################################
+#####################################################################################
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 2 of the License, or
@@ -26,23 +26,15 @@
 #
 # You should have received a copy of the GNU General Public License along
 # with this program. If not, see <https://www.gnu.org/licenses/>.
-###############################################################################
+#####################################################################################
 
-## Shellcheck Directives ##
+### Source function library ##
 # shellcheck source=/home/rick/bin/functionlib
-
-## Source function library ##
-
-if [[ -x ~/bin/functionlib ]]; then
-  source ~/bin/functionlib
-else
-  printf "\e[91mERROR:\e[0m functionlib not found!\n" >&2
-  exit 81
-fi
+source ~/bin/functionlib || { printf "\e[91mERROR:\e[0m Unable to source functionlib\n"; exit 1; }
 
 search_packages() {
 	# Based on code by Kris Occhipinti (with minor changes)
-	local package do_it
+	local package #do_it
 	package="$(
 		apt-cache pkgnames |
 			fzf -m --prompt="Enter Package Names: " \
@@ -50,22 +42,22 @@ search_packages() {
 				--preview-window 'top:75%' |
 				tr "\n" " "
 	)"
-	# package="${package// /}"	# remove trailing whitespace
+	package="${package// /}"	# remove trailing whitespace
 	[[ "$package" ]] || { printf "Nothing selected\n"; return; }
-	[[ "$package" ]] && read -n1 -rp "Install ${package}? [y/N]: " do_it
-	if [[ "${do_it,,}" != "y" ]]; then
-		printf "Installation declined.\n"
-		return
+	installed "$package" && { printf "%s is already installed.\n" "$package"; return 0; }
+	if default_no	"Install ${package}?"; then
+		sudo_login 2
+		printf "\n"
+		sudo apt install "$package"
+	else
+		printf "%s selected but not installed.\n" "$package"
 	fi
-	printf "\n"
-	sudo_login 2
-	sudo apt install "$package"
-	printf "\nDone\n"
+	return 0
 }
 
 main() {
   local -r script="${0##*/}"
-  local -r version="1.4.26097"
+  local -r version="1.5.26182"
   check_package fzf
   search_packages
   over_line "$script $version"
