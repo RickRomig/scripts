9288852144fa940f5d3d1d18968426c1f9ed4fce
Richard Romig <rick.romig@gmail.com>
2026-07-08 10:26:54 -0400

    update-clones.sh: moved iteration of clones array to loop_clones()

--- update-clones.sh
+++ update-clones.sh
@@ -1,36 +1,31 @@
 #!/usr/bin/env bash
-##########################################################################
+################################################################################
 # Script Name  : update-clones.sh
 # Description  : update cloned repositories using git pull
 # Dependencies : git
-# Arguments    : See help() function for available options.
+# Arguments    : None
 # Author       : Copyright © 2025 Richard B. Romig, Mosfanet
 # Email        : rick.romig@gmail | rick.romig@mymetronet.net
 # Created      : 13 Aug 2025
-# Last updated : 11 Apr 2026
+# Last updated : 08 Jul 2026
 # Comments     :
 # TODO (Rick)  :
 # License      : GNU General Public License, version 2.0
 # License URL  : https://github.com/RickRomig/scripts/blob/main/LICENSE
-##########################################################################
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
-# (at your option) any later version.
+################################################################################
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 2 of the License, or (at your option) any later version.
 #
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-# GNU General Public License for more details.
-##########################################################################
+# This program is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+# PARTICULAR PURPOSE. See the GNU General Public License for more details.
+################################################################################
 
 ## Source function library ##
 # shellcheck source=/home/rick/bin/functionlib
-source functionlib || { printf "\e[91mERROR:\e[0m Unable to source functionlib\n"; exit 1; }
+source ~/bin/functionlib || { printf "\e[91mERROR:\e[0m Unable to source functionlib\n"; exit 1; }
 
-## Functions ##
-
-# shellcheck disable=SC2088 # Tilde does not expand in quotes.
 update_clone() {
 	local -r clone="$1"
 	local -r repo_log="$2"
@@ -38,36 +33,43 @@ update_clone() {
 	[[ -d "$HOME/$clone" ]] && repo_dir="$HOME/$clone"
 	{
 		if [[ -d "$repo_dir" ]]; then
-			pushd "$repo_dir" || { EC="$E_POPD_PUSHD"; return; }
+			pushd "$repo_dir" || return "$E_POPD_PUSHD"
 			git checkout .
 			git pull
-			popd >/dev/null 2>&1 || { EC="$E_POPD_PUSHD"; return; }
+			popd >/dev/null 2>&1 || return "$E_POPD_PUSHD"
 			printf "\n"
 		else
-			printf "~/Downloads/%s ~\nHas not been cloned to this computer.\n~\n" "$clone"
+			printf "~ %s repository ~\nHas not been cloned to this computer.\n~\n" "$clone"
 		fi
 	} | tee -a "$repo_log"
+	return "$?"
 }
 
-main() {
-	EC=0
-	local clone clones
-  local -r script="${0##*/}"
-  local -r version="3.1.26101"
-	local -r log_dir="$HOME/.local/share/logs"
+loop_clones() {
+	local -r script="$1"
+	local -r version="$2"
+	local -r log_dir=~/.local/share/logs
 	local -r repo_log="$log_dir/repo-update.log"
+  local -r clones=(configs scripts i3wm-debian homepage)
+	local clone
 	[[ -d "$log_dir" ]] || mkdir -p "$log_dir"
-  clones=(configs scripts i3wm-debian homepage)
-  check_package git
-  printf "Updating cloned repositories...\n\n"
 	printf "%(%F %R)T (%s %s)\n" -1 "$script" "$version" > "$repo_log"
   for clone in "${clones[@]}"; do
 		update_clone "$clone" "$repo_log"
   done
-  over_line "$script $version"
-  exit "$EC"
+	return "$?"
 }
 
-## Execution ##
+main() {
+  local -r script="${0##*/}"
+  local -r version="4.0.26189"
+	local -i exit_code=0
+  check_package git
+  printf "Updating cloned repositories...\n\n"
+	loop_clones "$script" "$version"
+	exit_code="$?"
+  over_line "$script $version"
+  exit "$exit_code"
+}
 
 main "$@"
