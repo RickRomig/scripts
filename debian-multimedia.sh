8bf03b80b1c68e55efb16e16acc56e84c7aad642
Richard Romig <rick.romig@gmail.com>
2026-07-19 12:02:08 -0400

    debian-multimedia.sh: refactored set_sources() to write dmo.sources file instead of copying from files directory

--- debian-multimedia.sh
+++ debian-multimedia.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 ###############################################################################
 # Script Name  : debian-multimedia.sh
-# Description  : adds multimedia repository to Debian
+# Description  : adds multimedia repository to Debian Bookworm and Trixie
 # Dependencies : wget
 # Arguments    : None
 # Author       : Copyright © 2025, Richard B. Romig, Mosfanet
 # Email        : rick.romig@gmail | rick.romig@mymetronet.net
 # Created      : 28 Oct 2025
-# Last updated : 02 Jul 2026
+# Last updated : 19 Jul 2026
 # Comments     : Tested with Debian 12 (bookworm) & Debian 13 (trixie)
 # TODO (Rick)  :
 # License      : GNU General Public License, version 2.0
@@ -15,7 +15,8 @@
 ###############################################################################
 # This program is free software; you can redistribute it and/or modify it under
 # the terms of the GNU General Public License as published by the Free Software
-# Foundation; either version 2 of the License, or (at your option) any later version.
+# Foundation; either version 2 of the License, or (at your option) any later
+# version.
 #
 # This program is distributed in the hope that it will be useful, but WITHOUT
 # ANY WARRANTY; without even the implied warranty of# MERCHANTABILITY or
@@ -24,10 +25,8 @@
 ###############################################################################
 
 ## Source function library ##
-# shellcheck source=/home/rick/bin/functionlib
-source ~/bin/functionlib || { printf "\e[91mERROR:\e[0m Unable to source functionlib\n"; exit 1; }
-
-## Functions ##
+# shellcheck source=/home/rick/bin/functionlib.bash
+source ~/bin/functionlib.bash || { printf "\e[91mERROR:\e[0m Unable to source functionlib.bash\n"; exit 1; }
 
 # ShellCheck may incorrectly believe that code is unreachable if it's invoked by a variable name or in a trap.
 # shellcheck disable=SC2317 # Don't warn about unreachable commands in this function
@@ -45,15 +44,26 @@ install_keyring() {
 }
 
 set_sources() {
-	local distro="$1"
-	local -r source_list=/etc/apt/sources.list.d/dmo.sources
-	script_dir=$(dirname "$(readlink -f "${0}")")
+	local -r distro="$1"
+	local -r sources_file=/etc/apt/sources.list.d/dmo.sources
+	local -r dmo_key=/usr/share/keyrings/deb-multimedia-keyring.gpg
+	local -r sources_uri="https://www.deb-multimedia.org"
 	printf "Setting multimeda sources...\n"
-	if [[ ! -f "$script_dir/files/${distro}-dmo.sources" ]]; then
-		printf "%s not found.\n" "$script_dir/files/${distro}-dmo.sources"
-		return 2
-	fi
-	sudo cp -v "$script_dir/files/${distro}-dmo.sources" "$source_list"
+	tee $sources_file <<< \
+"Types: deb
+URIs: $sources_uri
+Suites: $distro
+Components: main non-free
+Signed-By: $dmo_key
+Enabled: yes
+
+# Backports
+Types: deb
+URIs: $sources_uri
+Suites: $distro-backports
+Components: main
+Signed-By: $dmo_key
+Enabled: yes"
 	printf "Debian mulitimedia repository sources set.\n"
 	return 0
 }
@@ -63,28 +73,26 @@ install_multimedia() {
 	distro="$(/usr/bin/lsb_release --codename --short)"
 	case "$distro" in
 		bookworm|trixie )
-			install_keyring
-			set_sources "$distro" ;;
+			sudo_login 2
+			check_package wget
+			install_keyring && set_sources "$distro" ;;
 		* )
 			printf "%s is not supported by this script.\n" "${distro^}"
+			return "$E_UNSUPPORTED"
 	esac
 	return "$?"
 }
 
 main() {
   local -r script="${0##*/}"
-  local -r version="2.7.26183"
+  local -r version="3.0.26200"
 	local -i exit_code=0
 	TMP_DIR=$(mktemp -qd) || die "Failed to create temporary directory." "$E_TEMP_DIR"
   trap cleanup EXIT
-	sudo_login 2
-	check_package wget
 	install_multimedia
 	exit_code="$?"
 	over_line "$script $version"
   exit "$exit_code"
 }
-
-## Execution ##
 
 main "$@"
