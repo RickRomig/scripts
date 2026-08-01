7738e6c72de5caa095e2516e49287a262c42c0cc
Richard Romig <rick.romig@gmail.com>
2026-08-01 10:44:38 -0400

    nosleep.sh: udpated local variables & return codes

--- nosleep.sh
+++ nosleep.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-##########################################################################
+###############################################################################
 # Script Name  : nosleep.sh
 # Description  : Script to disable sleep and hiberation on Debian-based systems
 # Dependencies : None
@@ -7,40 +7,24 @@
 # Author       : Copyright © 2023 Richard B. Romig, Mosfanet
 # Email        : rick.romig@gmail.com | rick.romig@mymetronet.net
 # Created      : 18 Jul 2023
-# Last updated : 25 Mar 2026
+# Updated      : 01 Aug 2026
+# Version      : 2.4.26213
 # Comments     :
 # TODO (Rick)  :
 # License      : GNU General Public License, version 2.0
 # License URL  : https://github.com/RickRomig/scripts/blob/main/LICENSE
-##########################################################################
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
-# (at your option) any later version.
+###############################################################################
+# This program is free software; you can redistribute it and/or modify  it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 2 of the License, or (at your option) any later version.
 #
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-# GNU General Public License for more details.
-##########################################################################
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+###############################################################################
 
-## Shellcheck Directives ##
-# shellcheck source=/home/rick/bin/functionlib
-
-## Source function library ##
-
-if [[ -x "$HOME/bin/functionlib" ]]; then
-  source "$HOME/bin/functionlib"
-else
-  printf "\e[91mERROR:\e[0m functionlib not found!\n" >&2
-  exit 81
-fi
-
-## Globbal variables #@
-
-EC=0
-
-## Functions ##
+# shellcheck source=/home/rick/bin/functionlib.bash
+source ~/bin/functionlib.bash || { printf "\e[91mERROR:\e[0m Unable to source functionlib.bash\n"; exit 1; }
 
 set_nosleep() {
   local script_dir
@@ -49,25 +33,25 @@ set_nosleep() {
   if [[ ! -f "$sed_file" ]]; then
     printf "A required file (%s) was not found.\n" "${sed_file##*/}" >&2
     printf "Operation could not continue.\n" >&2
-    EC="$E_FILENOTFOUND"
-    return
+    return "$E_FILENOTFOUND"
   fi
   sudo_login 2
   [[ -d /etc/systemd/sleep.conf.d ]] || sudo mkdir -p /etc/systemd/sleep.conf.d
   sudo cp -v /etc/systemd/sleep.conf /etc/systemd/sleep.conf.d/99-sleep.conf
   sudo sed -i -f "$sed_file" /etc/systemd/sleep.conf.d/99-sleep.conf
   printf "99-sleep.conf created.\n"
+  return 0
 }
 
 main() {
-  local script="${0##*/}"
-  local -r version="2.3.26084"
+  local -r script="${0##*/}"
+  local -r version="2.4.26213"
+	local -i exit_code=0
   printf "Disables sleep and hiberation on Debian-based systems.\n"
   set_nosleep
+  exit_code="$?"
   over_line "$script $version"
-  exit "$EC"
+  exit "$exit_code"
 }
-
-## Execution ##
 
 main "$@"
