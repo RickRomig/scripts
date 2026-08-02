f4efc1b0f30a58df91aa253f2d5235fe1a920469
Richard Romig <rick.romig@gmail.com>
2026-08-02 10:17:33 -0400

    03-bookworm2trixie.sh: updated exit codes

--- 03-bookworm2trixie.sh
+++ 03-bookworm2trixie.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-##########################################################################
+################################################################################
 # Script Name  : 03-bookworm2trixie.sh
 # Description  : Inplace upgrade from Debian 11 to Debian 12
 # Dependencies : none
@@ -7,28 +7,25 @@
 # Author       : Copyright © 2024 Richard B. Romig, Mosfanet
 # Email        : rick.romig@gmail.com | rick.romig@mymetronet.net
 # Created      : 06 Jun 2025
-# Last updated : 26 Jun 2026
+# Updated      : 02 Aug 2026
+# Version      :
 # Comments     : Cleanup after upgrade to Debian 13.
 # TODO (Rick)  :
 # License      : GNU General Public License, version 2.0
 # License URL  : https://github.com/RickRomig/scripts/blob/main/LICENSE
-##########################################################################
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
-# (at your option) any later version.
+################################################################################
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 2 of the License, or (at your option) any later
+# version.
 #
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-# GNU General Public License for more details.
-##########################################################################
+# This program is distributed in the hope that it will be useful,# but WITHOUT
+# ANY WARRANTY; without even the implied warranty of# MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+###############################################################################
 
-## Source function library ##
-# shellcheck source=/home/rick/bin/functionlib
-source ~/bin/functionlib || { printf "\e[91mERROR:\e[0m Unable to source functionlib\n"; exit 1; }
-
-## Functions ##
+# shellcheck source=/home/rick/bin/functionlib.bash
+source ~/bin/functionlib.bash || { printf "\e[91mERROR:\e[0m Unable to source functionlib.bash\n"; exit 1; }
 
 check_file1() {
 	[[ -f "$HOME/01-upgrade" ]] && return "$TRUE" || return "$FALSE"
@@ -46,6 +43,7 @@ version_info() {
 	printf "Version information:\n"
 	lsb_release --all 2>/dev/null
 	printf "%-16s%s\n" "Version:" "$(< /etc/debian_version)"
+  return 0
 }
 
 clean_up() {
@@ -57,28 +55,31 @@ clean_up() {
 	sudo apt --purge autoremove
 	printf "\nThere may be programs and applications that need to be reinstalled or updated.\n"
 	rm -v "$HOME/01-upgrade" "$HOME/02-sources"
+  return 0
 }
 
 modernize_sources() {
 	printf "Modernizing sources...\n"
+	sudo_login 2
 	[[ -f "/etc/apt/sources.list.d/debian.sources" ]] || sudo apt modernize-sources
+  return 0
 }
 
 main() {
 	local script="${0##*/}"
-	local version="1.8.26177"
-	local updated="26 Jun 2026"
+	local version="1.9.26214"
+	local updated="02 Aug 2026"
 	local exit_code=0
-	check_file1 || die "01-bookworm2trixie.sh and 02-bookworm2trixie.sh must be run first." "$E_INFO"
-	check_file2 || die "02-bookworm2trixie.sh hasn't been run." "$E_INFO"
 	version_info
+	check_file1 || die "01-bookworm2trixie.sh and 02-bookworm2trixie.sh must be run first." "$E_FILENOTFOUND"
+	check_file2 || die "02-bookworm2trixie.sh hasn't been run." "$E_FILENOTFOUND"
 	if check_codename; then
-		printf "%sDebian inplace upgrade was successful!%s\n" "$orange" "$normal"
+		printf "%sDebian in-place upgrade was successful!%s\n" "$orange" "$normal"
 		clean_up
 		modernize_sources
 	else
-		printf "Debian inplace upgrade failed.\n" >&2
-		printf "Backup the home directory and any important files, then install from the ISO.\n" >&2
+		printf "%sDebian in-place upgrade failed.%s\n" "$orange" "$normal" >&2
+		printf "Backup the home directory and any important files, then install from the most current netinstall ISO.\n" >&2
 		exit_code="$E_INSTALLATION"
 	fi
 	over_line "$script $version ($updated)"
