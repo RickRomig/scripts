54d6b79125c48fca5a3f087a4ccaa01a4cd55ae7
Richard Romig <rick.romig@gmail.com>
2026-08-02 10:16:09 -0400

    02-bookworm2trixie.sh: main() - moved check_codename() to version_info()

--- 02-bookworm2trixie.sh
+++ 02-bookworm2trixie.sh
@@ -7,28 +7,25 @@
 # Author       : Copyright © 2025 Richard B. Romig, Mosfanet
 # Email        : rick.romig@gmail.com | rick.romig@mymetronet.net
 # Created      : 06 Jun 2025
-# Last updated : 27 Jun 2026
+# Updated      : 02 Aug 2026
+# Version      : 1.10.26214
 # Comments     : This scripts updates sources.list & backports.list.
 # TODO (Rick)  :
 # License      : GNU General Public License, version 2.0
 # License URL  : https://github.com/RickRomig/scripts/blob/main/LICENSE
-##########################################################################
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
-# (at your option) any later version.
+###############################################################################
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 2 of the License, or (at your option) any later
+# version.
 #
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-# GNU General Public License for more details.
-##########################################################################
+# This program is distributed in the hope that it will be useful,# but WITHOUT
+# ANY WARRANTY; without even the implied warranty of# MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+###############################################################################
 
-## Source function library ##
-# shellcheck source=/home/rick/bin/functionlib
-source ~/bin/functionlib || { printf "\e[91mERROR:\e[0m Unable to source functionlib\n"; exit 1; }
-
-## Functions ##
+# shellcheck source=/home/rick/bin/functionlib.bash
+source ~/bin/functionlib.bash || { printf "\e[91mERROR:\e[0m Unable to source functionlib.bash\n"; exit 1; }
 
 check_file1() {
 	[[ -f "$HOME/01-upgrade" ]] && return "$TRUE" || return "$FALSE"
@@ -43,9 +40,11 @@ check_codename() {
 }
 
 version_info() {
-	printf "Version information:\n"
+	printf "%sVersion information%s:\n" "$orange" "$normal"
 	lsb_release --all 2>/dev/null
 	printf "%-16s%s\n" "Version:" "$(< /etc/debian_version)"
+	check_codename || die "Only upgrade from Debian 12 Bookworm is supported." "$E_UNSUPPORTED"
+  return 0
 }
 
 sources_list() {
@@ -53,7 +52,9 @@ sources_list() {
 	local list_path="/etc/apt/"
 	local src_list="sources.list"
 	sudo_login 2
+	printf "%sUpdatings sources lists...%s\n" "$orange" "$normal"
 	sudo sed -i.bak 's/http:/https:/g;/ftp/s/https:/http:/;s/bookworm/trixie/g' "$list_path/$src_list"
+  return 0
 }
 
 backports_list() {
@@ -61,48 +62,48 @@ backports_list() {
 	local list_path="/etc/apt/sources.list.d"
 	local old_name="bookworm-backports.list"
 	local new_name="trixie-backports.list"
+	printf "%sUpdating backports sources list...%s\n" "$orange" "$normal"
 	if [[ ! -f "$list_path/$old_name" ]]; then
 		printf "%s/%s does not exist.\n" "$list_path" "$old_name"
-		return
+		return 0
 	fi
 	sudo_login 2
 	sudo sed -i.bak 's/http:/https:/;s/bookworm/trixie/' "$list_path/$old_name"
 	# sudo cp -v "$list_path/$old_name" "$list_path/$old_name.bak"
 	# find "$list_path" -name "*.list" -exec sudo sed -i.bak 's/bookworm/trixie/g' {} \;
 	sudo mv -v "$list_path/$old_name" "$list_path/$new_name"
+  return 0
 }
 
 upgrade_debian() {
 	# Cleans and updates apt cache, then upgrades to Debian 13.
+	printf "%sUpgrading to Debian 13 Trixie...%s\n" "$orange" "$normal"
+	sudo_login 2
 	sudo apt clean
 	sudo apt update
 	printf "%sUpgrade without new packages...%s\b" "$orange" "$normal"
 	sudo apt upgrade --without-new-pkgs
 	printf "%sFull upgrade...%s\n" "$orange" "$normal"
 	sudo apt full-upgrade
+	printf "%sDebian 13 Trixie installed.%s\n" "$orange" "$normal"
+  return 0
 }
 
 main() {
-	local script="${0##*/}"
-	local version="1.9.26178"
-	local updated="27 Jun 2026"
-	check_file1 || die "01-bookworm2trixie.sh must be run first." "$E_INFO"
-	check_file2 && die "This script has already been run." "$E_INFO"
+	local -r script="${0##*/}"
+	local -r version="1.10.26214"
+	local -r updated="02 Aug 2026"
 	check_package apt-transport-https
 	version_info
-	check_codename || die "Unsupported Debian version." "$E_INFO"
-	printf "%sUpdatings source lists...%s\n" "$orange" "$normal"
+	check_file1 || die "01-bookworm2trixie.sh must be run first." "$E_FILENOTFOUND"
+	check_file2 && die "This script has already been run." "$E_FILE_EXISTS"
 	sources_list
 	backports_list
-	printf "%sUpgrading to Debian 13...%s\n" "$orange" "$normal"
 	upgrade_debian
-	printf "%sDebian 13 Trixie installed.%s\n" "$orange" "$normal"
 	printf "Reboot and run 03-bookworm2trixie.sh\n"
 	touch "$HOME/02-sources"
 	over_line "$script $version ($updated)"
 	exit
 }
-
-## Execution ##
 
 main "$@"
