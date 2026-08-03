bedc20b1a7db2b00db4a44113bf717261bc8b3ee
Richard Romig <rick.romig@gmail.com>
2026-08-03 10:16:41 -0400

    rm-duplicates.sh: updated variables

--- rm-duplicates.sh
+++ rm-duplicates.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-##########################################################################
+###############################################################################
 # Script Name  : rm-duplicates.sh
 # Description  : Find file sizes & use to potentially ID duplicate files
 # Dependencies :
@@ -7,80 +7,59 @@
 # Author       : Copyright © 2026, Richard B. Romig, Mosfanet
 # Email        : rick.romig@gmail | rick.romig@mymetronet.net
 # Created      : 19 Feb 2026
-# Last updated : 26 Mar 2026
-# Version      : 1.1.26085
+# Updated      : 03 Aug 2026
+# Version      : 0.2.26215 (ALPHA)
 # Comments     :
-# TODO (Rick)  :
+# TODO (Rick)  : Still needs work.
 # License      : GNU General Public License, version 2.0
 # License URL  : https://github.com/RickRomig/scripts/blob/main/LICENSE
-##########################################################################
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
-# (at your option) any later version.
+###############################################################################
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 2 of the License, or (at your option) any later
+# version.
 #
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-# GNU General Public License for more details.
-##########################################################################
+# This program is distributed in the hope that it will be useful,# but WITHOUT
+# ANY WARRANTY; without even the implied warranty of# MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+###############################################################################
 
-## Shellcheck Directives ##
-# shellcheck source=/home/rick/bin/functionlib
+# shellcheck source=/home/rick/bin/functionlib.bash
+source ~/bin/functionlib.bash || { printf "\e[91mERROR:\e[0m Unable to source functionlib.bash\n"; exit 1; }
 
-## Source function library ##
-
-if [[ -x "$HOME/bin/functionlib" ]]; then
-  source "$HOME/bin/functionlib"
-else
-  printf "\e[91mERROR:\e[0m functionlib not found!\n" >&2
-  exit 81
-fi
-
-## Global Variables ##
-
-tmp_file=$(mktemp -q) || die "Failed to create temporary file." "$E_TEMP_FILE"
-
-## Functions ##
+TMP_FILE=$(mktemp -q) || die "Failed to create temporary file." "$E_TEMP_FILE"
 
 help() {
 	local -r script="$1"
-	local -r updated="19 Feb 2026"
+  local -r version="$2"
+  local -ri errcode="${3:-1}"
+	local -r updated="03 Aug 2026"
   cat << _HELP_
 	${orange}$script${normal} $version, Updated: $updated
 	Run this script from in the directory you want to find dupliate files.
 	Have a second terminal window open in which to compare suspected duplicate.
 	Remove duplicates as needed.
 _HELP_
-  exit
+  exit "$errcode"
 }
 
-# shellcheck disable=SC2317
-# Don't warn about unreachable commands in this function
+# shellcheck disable=SC2317 # Don't warn about unreachable commands in this function
 # ShellCheck may incorrectly believe that code is unreachable if it's invoked by variable name or in a trap.
 cleanup() {
-  [[ -f "$tmp_file" ]] && rm -f "$tmp_file"
-}
-
-find_sizes() {
-	awk '{print $5, $9}' < <(find . -type f -size +1M -exec ls -l {} \;) | sort -n > "$tmp_file"
-}
-
-filter_duplicates() {
-	awk 'NR==FNR{count[$1]++; next} count[$1]>1' "$tmp_file" "$tmp_file" | less -N
+  [[ -f "$TMP_FILE" ]] && rm -f "$TMP_FILE"
 }
 
 main() {
   local -r script="${0##*/}"
-  local -r version="1.1.26085"
+  local -r version="0.2.26215"
   trap cleanup EXIT
-  [[ "$1" == "-h" || "$1" == "--help" ]] && help "$script"
-  find_sizes
-  filter_duplicates
+  [[ "$1" == "-h" || "$1" == "--help" ]] && help "$script" "$version" 0
+  # find file sizes
+  awk '{print $5, $9}' < <(find . -type f -size +1M -exec ls -l {} \;) | sort -n > "$TMP_FILE"
+  # filter duplicates
+  awk 'NR==FNR{count[$1]++; next} count[$1]>1' "$TMP_FILE" "$TMP_FILE" | less -N
   over_line "$script $version"
 	exit
 }
-
-## Execution ##
 
 main "$@"
