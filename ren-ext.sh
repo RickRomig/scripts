14dd0efc437d214c41f567149c38895d82441aec
Richard Romig <rick.romig@gmail.com>
2026-08-02 20:15:20 -0400

    ren-ext.sh:  updated local variables, return codes

--- ren-ext.sh
+++ ren-ext.sh
@@ -7,37 +7,31 @@
 # Author       : Copyright (C) 2017, Richard Romig
 # Email        : rick.romig@gmail.com | rick.romig@mymetronet.com
 # Created      : 2017
-# Updated      : 30 May 2026
+# Updated      : 02 Aug 2026
+# Version      : 4.2.26214
 # Comments     : File extensions are not case sensitive, but will change to lowercase.
 # TODO (Rick)  :
 # License      : GNU General Public License, version 2.0
 # License URL  : https://github.com/RickRomig/scripts/blob/main/LICENSE
-##########################################################################
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
-# (at your option) any later version.
+###############################################################################
+# This program is free software; you can redistribute it and/or modify it  under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 2 of the License, or (at your option) any later
+# version.
 #
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-# GNU General Public License for more details.
-##########################################################################
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+###############################################################################
 
-## Load function library ##
-# shellcheck source=/home/rick/bin/functionlib
-source functionlib || { printf "\e[91mERROR:\e[0m Unable to source functionlib\n"; exit 1; }
-
-## Global Variables ##
-
-readonly script="${0##*/}"
-readonly version="4.2.26150"
-
-## Functions ##
+# shellcheck source=/home/rick/bin/functionlib.bash
+source functionlib.bash || { printf "\e[91mERROR:\e[0m Unable to source functionlib.bash\n"; exit 1; }
 
 help() {
-	local errcode="${1:-1}"
-	local updated="30 May 2026"
+  local -r script="$1"
+  local -r version="$2"
+	local -ri errcode="${3:-1}"
+	local -r updated="30 May 2026"
 	cat << _HELP_
 ${orange}$script${normal} $version ($updated)
 Bulk renames file extensions in the current diectory.
@@ -65,17 +59,18 @@ rename_extension() {
     * )
       rename -v 's/(\.[A-Z]+$)/lc($1)/gei' -- *.*[A-Z]*
   esac
+  return 0
 }
 
 main() {
-  local extension="$1"
+  local -r script="${0##*/}"
+  local -r version="4.2.26214"
+  local -r extension="$1"
   check_package rename
-  [[ "$extension" == "-h" || "$extension" == "--help" ]] && help 0
+  [[ "$extension" == "-h" || "$extension" == "--help" ]] && help "$script" "$version" 0
   rename_extension "$extension"
   over_line "$script $version"
   exit
 }
-
-## Execution ##
 
 main "$@"
