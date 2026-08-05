c898ffe681388846f852c6b3b7bd3c92aa38460f
Richard Romig <rick.romig@gmail.com>
2026-08-05 10:39:47 -0400

    umount-usb.sh: updated local variablles & return codes

--- umount-usb.sh
+++ umount-usb.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/env bash
-##########################################################################
+###############################################################################
 # Script Name  : umount-usb.sh
 # Description  : script to unmount a USB drive in the /media/<user> directory
 # Dependencies : fzf
@@ -7,36 +7,25 @@
 # Author       : Copyright © 2025, Richard B. Romig, Mosfanet
 # Email        : rick.romig@gmail.com | rick.romig@mymetronet.com
 # Created      : 12 Mar 2025
-# Last updated : 27 Mar 2026
+# Last updated : 05 Aug 2026
+# Version      ; 2.3.26217
 # Comments     : Only unmounts devices belonging to the current user.
 # TODO (Rick)  :
 # License      : GNU General Public License, version 2.0
 # License URL  : https://github.com/RickRomig/scripts/blob/main/LICENSE
-##########################################################################
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
-# (at your option) any later version.
+###############################################################################
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 2 of the License, or (at your option) any later
+# version.
 #
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-# GNU General Public License for more details.
-##########################################################################
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+###############################################################################
 
-## Shellcheck Directives ##
-# shellcheck source=/home/rick/bin/functionlib
-
-# Load function library
-
-if [[ -x "$HOME/bin/functionlib" ]]; then
-  source "$HOME/bin/functionlib"
-else
-  printf "\e[91mERROR:\e[0m functionlib not found!\n" >&2
-  exit 81
-fi
-
-## Functions ##
+# shellcheck source=/home/rick/bin/functionlib.bash
+source ~/bin/functionlib.bash || { printf "\e[91mERROR:\e[0m Unable to source functionlib.bash\n"; exit 1; }
 
 check_usb() {
 	[[ $(grep 'usb' <(lsblk -S -o TRAN)) = *usb* ]] && return "$TRUE" || return "$FALSE"
@@ -44,26 +33,26 @@ check_usb() {
 
 unmount_usb() {
 	local user usb_drive
-	check_usb || { printf "No USB drive connected!\n" >&2; EC="$E_DRIVE_ERROR"; return; }
+	check_usb || { printf "No USB drive connected!\n" >&2; return "$E_DRIVE_ERROR"; }
 	user=$(whoami)
 	usb_drive=$(fzf --height 40% --reverse --prompt "Select the USB drive to unmount: " < <(find /media/"$user" -maxdepth 1 -type d -user "$user"))
-	[[ "$usb_drive" ]] || { printf "No USB drives mounted or selected!\n" >&2; EC="$E_DRIVE_ERROR"; return; }
+	[[ "$usb_drive" ]] || { printf "No USB drives mounted or selected!\n" >&2; return "$E_DRIVE_ERROR"; }
 	umount -l "$usb_drive"
 	printf "\"%s\" unmounted.\n" "$usb_drive"
 	[[ -d "$usb_drive" ]] && rmdir "$usb_drive"
 	pgrep -x nemo >/dev/null && nemo --quit	# If Nemo is running, close it.
+	retunn "$?"
 }
 
 main() {
-	EC=0
 	local -r script="${0##*/}"
-	local -r version="2.2.26086"
+	local -r version="2.3.26217"
+	local -i exit_code=0
 	check_package fzf
 	unmount_usb
+	exit_code "$?"
 	over_line "$script $version"
-	exit "$EC"
+	exit "$exit_code"
 }
-
-## Execution ##
 
 main "$@"
