479044d37308ad251628e29c05d00df6c9afc24e
Richard Romig <rick.romig@gmail.com>
2026-08-06 19:11:23 -0400

    devtoolinst.sh: sourced functionlib.bash

--- devtoolinst.sh
+++ devtoolinst.sh
@@ -7,28 +7,24 @@
 # Author       : Copyright (C), Richard Romig
 # Email        : rick.romig@gmail.com | rick.romig@mymetronet.net
 # Created      : 01 Jan 2017
-# Updated      : 21 May 2026
+# Updated      : 06 Aug 2026
+# Version      : 2.3.26218
 # Comment      :
 # License      : GNU General Public License, version 2.0
 # License URL  : https://github.com/RickRomig/scripts/blob/main/LICENSE
-#####################################################################
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
-# (at your option) any later version.
+##############################################################################
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 2 of the License, or (at your option) any later
+# version.
 #
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-# GNU General Public License for more details.
-#####################################################################
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+###############################################################################
 
-## Source function library ##
-
-# shellcheck source=/home/rick/bin/functionlib
-source ~/bin/functionlib || { printf "\e[91mERROR:\e[0m Unable to source functionlib\n"; exit 1; }
-
-## Functions ##
+# shellcheck source=/home/rick/bin/functionlib.bash
+source ~/bin/functionlib.bash || { printf "\e[91mERROR:\e[0m Unable to source functionlib.bash\n"; exit 1; }
 
 install_tools() {
   local package packages
@@ -42,16 +38,15 @@ install_tools() {
       sudo apt-get install -y "$package"
 		fi
 	done
+	return 0
 }
 
 main() {
 	local -r script="${0##*/}"
-	local -r version="2.3.26141"
+	local -r version="2.3.26218"
   install_tools
 	over_line "$script $version"
   exit
 }
-
-## Execution ##
 
 main "$@"
