8855e70e9d4b284d786b6b3b024f135a95ef491a
Richard Romig <rick.romig@gmail.com>
2026-08-06 19:56:31 -0400

    dos2linux.sh: sourced functionlib.bash

--- dos2linux.sh
+++ dos2linux.sh
@@ -7,42 +7,40 @@
 # Author       : Copyright (C) 2019, Richard Romig, Mosfanet
 # Email        : rick.romig@gmail.com | rick.romig@mymetronet.net
 # Created      : 24 Jan 2019
-# Updated      : 02 Jul 2026
+# Updated      : 06 Aug 2026
+# Version      : 2.3.26218
 # TODO (Rick)  :
 # Comment      : removes DOS carriage return ('\r') characters
 #              : Similar in function to dos2unix.
 # License      : GNU General Public License, version 2.0
 # License URL  : https://github.com/RickRomig/scripts/blob/main/LICENSE
-###############################################################################
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
-# (at your option) any later version.
+##############################################################################
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 2 of the License, or (at your option) any later
+# version.
 #
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-# GNU General Public License for more details.
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
 ###############################################################################
 
-## Source function library ##
-# shellcheck source=/home/rick/bin/functionlib
-source ~/bin/functionlib || { printf "\e[91mERROR:\e[0m Unable to source functionlib\n"; exit 1; }
-
-## Functions ##
+# shellcheck source=/home/rick/bin/functionlib.bash
+source ~/bin/functionlib.bash || { printf "\e[91mERROR:\e[0m Unable to source functionlib.bash\n"; exit 1; }
 
 convert_file() {
-  local filename="$1"
+  local -r filename="$1"
   printf "Removing the carriage return characters from %s\n" "$filename"
   printf "and creating a backup (%s.bak) of the orginal file."  "$filename"
   sed -i.bak 's/\r//g' "$filename"
+  return 0
 }
 
 main() {
   local -r script="${0##*/}"
-  local -r version="2.3.26183"
+  local -r version="2.3.26218"
   local filename="$1"
-  local exit_code=0
+  local -i exit_code=0
   printf "Converts a DOS text file to Linux format by removing carriage returns.\n"
   [[ "$#" -eq 0 ]] && read -rp "Enter a DOS text file to process: " filename
   if [[ -f "$filename" ]]; then
@@ -54,7 +52,5 @@ main() {
   over_line "$script $version"
   exit "$exit_code"
 }
-
-## Execution ##
 
 main "$@"
