8ff93a5a19cec1648b5b22e66e4c27676d75f250
Richard Romig <rick.romig@gmail.com>
2026-08-06 17:41:08 -0400

    checkinit.sh: updated GPL license info

--- checkinit.sh
+++ checkinit.sh
@@ -7,24 +7,22 @@
 # Author       : Copyright (C) 2022, Richard B. Romig, Mosfanet
 # Email        : rick.romig@gmail.com | rick.romig@mymetronet.net
 # Created      : 07 Sep 2022
-# Updated      : 03 May 2026
+# Updated      : 06 Aug 2026
+# Version      : 2.2.26218
 # Comments     :
 # TODO (Rick)  :
 # License      : GNU General Public License, version 2.0
 # License URL  : https://github.com/RickRomig/scripts/blob/main/LICENSE
-##########################################################################
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
-# (at your option) any later version.
+###############################################################################
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 2 of the License, or (at your option) any later
+# version.
 #
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-# GNU General Public License for more details.
-##########################################################################
-
-## Functions ##
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+###############################################################################
 
 get_init() {
 	if [[ $(cat /proc/1/comm) == "systemd" ]] ; then
@@ -40,17 +38,16 @@ get_init() {
 	else
 		echo "Undetermined"
   fi
+	return 0
 }
 
 main() {
 	local -r script="${0##*/}"
-	local -r version="2.2.26123"
+	local -r version="2.2.26218"
 	local -r short_line="---"
 	printf "Init System: %s\n" "$(get_init)"
 	printf "%s\n%s %s\n" "$short_line" "$script" "$version"
 	exit
 }
-
-## Execution ##
 
 main "$@"
