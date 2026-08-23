462b2795478165ff9938af3b8d330ec513eaebc5
Richard Romig <rick.romig@gmail.com>
2026-08-23 13:24:41 -0400

    functionlib.bash: removed functions for for mount/unmout server and NAS; added create_tmp() and cleanup() for temporary files & directories

--- functionlib.bash
+++ functionlib.bash
@@ -531,80 +531,6 @@ in_repos() {
   [[ "$(awk '/Package:/ {print $NF}' < <(apt-cache show "$package" 2>/dev/null))" ]] && return "$TRUE" || return "$FALSE"
 }
 
-mount_server() {
-  local -r server_ip="11"
-  local -r share="HP-6005"
-  ping -c3 "$LOCALNET.$server_ip" >/dev/null 2>&1 || die "$share at $LOCALNET.$server_ip is not online." "$E_NETWORK"
-  if [[ -d "$HOME/mnt/$share/" ]]; then
-    mounted=$(grep "$share" <(mount))
-    if [[ -z "$mounted" ]]; then
-      sshfs -o follow_symlinks rick@"$LOCALNET.$server_ip:/home/rick" "$HOME/mnt/$share/"
-      echo "$share has been mounted."
-    else
-      echo "$share is already mounted"
-    fi
-  else
-    # Create the mount point.
-    mkdir -p "$HOME/mnt/$share/"
-    sshfs -o follow_symlinks rick@"$LOCALNET.$server_ip:/home/rick" "$HOME/mnt/$share/"
-    echo "$share has been created and mounted."
-  fi
-	return 0
-}
-
-# shellcheck disable=SC2154
-# var is referenced but not assigned
-unmount_server() {
-  local mounted
-  local -r share="HP-6005"
-  mounted=$(grep "$share" < <(mount))
-  if [[ -n "$mounted" ]]; then
-    fusermount -u "$HOME/mnt/$share"
-    [[ -d "$TMP_DIR" ]] && rm -rf "$TMP_DIR"  # should be declered as a global variable in the calling script
-    [[ -f "$TMP_FILE" ]] && rm -f "$TMP_FILE" # should be declered as a global variable in the calling script
-    printf "%s has been unmounted.\n" "$share"
-  else
-    printf "%s is not mounted.\n" "$share" >&2
-  fi
-	return 0
-}
-
-mount_nas() {
-  local server_ip share mounted
-  server_ip="4"
-  share="NASD97167"
-  ping -c3 "$LOCALNET.$server_ip" > /dev/null 2>&1 || die "$share at $LOCALNET.$server_ip is not online." "$E_NETWORK"
-  if [[ -d "$HOME/mnt/$share/" ]]; then
-    mounted=$(grep "$share" <(mount))
-    if [[ -z "$mounted" ]]; then
-      sshfs -o follow_symlinks rick@"$LOCALNET.$server_ip:" "$HOME/mnt/$share/"
-      echo "$share has been mounted."
-    else
-      echo "$share is already mounted"
-    fi
-  else
-    mkdir -p "$HOME/mnt/$share/"    # Create the mount point.
-    sshfs -o follow_symlinks rick@"$LOCALNET.$server_ip:" "$HOME/mnt/$share/"
-    echo "$share has been created and mounted."
-  fi
-	return 0
-}
-
-unmount_nas() {
-  local mounted share
-  share="NASD97167"
-  mounted=$(grep "$share" < <(mount))
-  if [[ -n "$mounted" ]]; then
-    fusermount -u "$HOME/mnt/$share"
-    [[ -d "$TMP_DIR" ]] && rm -rf "$TMP_DIR"  # should be declered as a global variable in the calling script
-    [[ -f "$TMP_FILE" ]] && rm -f "$TMP_FILE" # should be declered as a global variable in the calling script
-    printf "%s has been unmounted.\n" "$share"
-  else
-    printf "%s is not mounted.\n" "$share" >&2
-  fi
-	return 0
-}
-
 assign_cfg_repo() {
 	local local_host="${HOSTNAME:-$(hostname)}"
 	local repo_dir="$HOME/Downloads/configs"
@@ -755,4 +681,28 @@ reboot_system() {
     printf "\n%s Please reboot your system as soon as practical.\n" "$RED_WARNING"
   fi
 	return 0
+}
+
+create_tmp() {
+	tmp_type="$1"
+	case "$tmp_type" in
+		dir )
+			TMP_DIR=$(mktemp -d) || die "Failed to create temporary directory." "$E_TEMP_DIR" ;;
+		file )
+			TMP_FILE=$(mktemp) || die "Failed to create temporary file." "$E_TEMP_FILE" ;;
+		both )
+			TMP_DIR=$(mktemp -d) || die "Failed to create temporary directory." "$E_TEMP_DIR"
+			TMP_FILE=$(mktemp) || die "Failed to create temporary file." "$E_TEMP_FILE" ;;
+		* )
+			die "%s Invalid option. No temporary file/directory created." "$E_INVALID_ARG"
+	esac
+	trap cleanup EXIT
+  return 0
+}
+
+# ShellCheck may incorrectly believe that code is unreachable if it's invoked by variable name or in a trap.
+# shellcheck disable=SC2317 # Don't warn about unreachable commands in this function
+cleanup() {
+	[[ -f "$TMP_FILE" ]] && rm -f "$TMP_FILE"
+	[[ -d "$TMP_DIR" ]] && rm -rf "$TMP_DIR"
 }
