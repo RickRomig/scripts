d3070f313f20edb176e2c62b872c2555b1bedbe3
Richard Romig <rick.romig@gmail.com>
2026-09-05 09:25:19 -0400

    scriptarchive.sh: updated variable declarations and tabs

--- cronjobs/scriptarchive.sh
+++ cronjobs/scriptarchive.sh
@@ -27,52 +27,52 @@
 ##########################################################################
 
 archive_scripts() {
-  local arc_dir="$1"
-  local log_dir="$2"
-  local log_file="$3"
-  local -r err_log="error.log"
-  local archive
-  archive="$(date +%y%m%d)-scripts.tar.gz"
-  # Create archive and log directories if they don't exist
-  [[ -d "$arc_dir" ]] || mkdir -p "$arc_dir"
-  [[ -d "$log_dir" ]] || mkdir -p "$log_dir"
-  # Remove old error log
-  [[ -f "$arc_dir/$err_log" ]] && rm -f "$arc_dir/$err_log" >/dev/null 2>&1
-  # Create archive of the ~/bin directory and write results to log file
-  {
-    printf "%(%a|%F|%R)T|"
-    if /usr/bin/tar --exclude='.git' -hcpzf "$arc_dir/$archive" -C "$HOME" bin 2> "$arc_dir/$err_log"; then
-      printf "successful\n"
-      printf "Script Archive successful - %(%F)T\n" >> "$arc_dir/$err_log"
-    else
-      printf "had errors\n"
-      printf "Script Archive had errors  - %(%F)T\n" >> "$arc_dir/$err_log"
-    fi
-  } >> "$log_dir/$log_file" 2> "$arc_dir/$err_log"
+	local -r arc_dir="$1"
+	local -r log_dir="$2"
+	local -r log_file="$3"
+	local -r err_log="error.log"
+	local archive
+	archive="$(date +%y%m%d)-scripts.tar.gz"
+	# Create archive and log directories if they don't exist
+	[[ -d "$arc_dir" ]] || mkdir -p "$arc_dir"
+	[[ -d "$log_dir" ]] || mkdir -p "$log_dir"
+	# Remove old error log
+	[[ -f "$arc_dir/$err_log" ]] && rm -f "$arc_dir/$err_log" >/dev/null 2>&1
+	# Create archive of the ~/bin directory and write results to log file
+	{
+		printf "%(%a|%F|%R)T|"
+		if /usr/bin/tar --exclude='.git' -hcpzf "$arc_dir/$archive" -C "$HOME" bin 2> "$arc_dir/$err_log"; then
+			printf "successful\n"
+			printf "Script Archive successful - %(%F)T\n" >> "$arc_dir/$err_log"
+		else
+			printf "had errors\n"
+			printf "Script Archive had errors  - %(%F)T\n" >> "$arc_dir/$err_log"
+		fi
+	} >> "$log_dir/$log_file" 2> "$arc_dir/$err_log"
 }
 
 cleanup() {
-  local arc_dir="$1"
-  local log_dir="$2"
-  local log_file="$3"
-  local log_len
-  # Remove oldest log entries if more than 30 entries
-  log_len=$(wc -l < "$log_dir/$log_file")
-  while [[ "$log_len" -gt 30 ]]; do
-    sed -i '1d' "$log_dir/$log_file"
-    (( log_len-- ))
-  done
-  # Remove archives older than two years (730 days).
-  find "$arc_dir"/ -maxdepth 1 -type f -mtime +730 -exec rm {} +
+	local -r arc_dir="$1"
+	local -r log_dir="$2"
+	local -r log_file="$3"
+	local -i log_len
+	# Remove oldest log entries if more than 30 entries
+	log_len=$(wc -l < "$log_dir/$log_file")
+	while [[ "$log_len" -gt 30 ]]; do
+		sed -i '1d' "$log_dir/$log_file"
+		(( log_len-- ))
+	done
+	# Remove archives older than two years (730 days).
+	find "$arc_dir"/ -maxdepth 1 -type f -mtime +730 -exec rm {} +
 }
 
 main() {
-  local -r arc_dir="$HOME/Downloads/archives/scripts"
-  local -r log_dir="$HOME/.local/share/logs"
-  local -r log_file="script-archive.log"
-  archive_scripts "$arc_dir" "$log_dir" "$log_file"
-  cleanup "$arc_dir" "$log_dir" "$log_file"
-  exit
+	local -r arc_dir="$HOME/Downloads/archives/scripts"
+	local -r log_dir="$HOME/.local/share/logs"
+	local -r log_file="script-archive.log"
+	archive_scripts "$arc_dir" "$log_dir" "$log_file"
+	cleanup "$arc_dir" "$log_dir" "$log_file"
+	exit
 }
 
 main "$@"
