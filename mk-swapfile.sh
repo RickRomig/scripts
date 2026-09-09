33a7c55d34a1bf673e467249a753329bee1b4cad
Richard Romig <rick.romig@gmail.com>
2026-09-09 11:32:13 -0400

    mk-swapfile.sh: udated indentation; create_swapfile() runs process_swapfile() on success of swapfile creation

--- mk-swapfile.sh
+++ mk-swapfile.sh
@@ -7,8 +7,8 @@
 # Author       : Copyright © 2025 Richard B. Romig, Mosfanet
 # Email        : rick.romig@gmail.com | rick.romig@mymetronet.net
 # Created      : 27 Jan 2025
-# Updated      : 31 Jul 2026
-# Version      : 2.3.26212
+# Updated      : 09 Sep 2026
+# Version      : 2.4.26252
 # Comments     : creates a swap file if no other swap exists.
 #              : Disable old swap and comment out in /etc/fstab
 #              : User is prompted to provide size of swap file in GB (integer value)
@@ -30,9 +30,37 @@
 source ~/bin/functionlib.bash || { printf "\e[91mERROR:\e[0m Unable to source functionlib.bash\n"; exit 1; }
 
 swap_exists() {
-  local myswap
-  myswap=$(awk '/file/ || /partition/ {print $1}' /proc/swaps)
-  [[ "$myswap" ]] && return "$TRUE" || return "$FALSE"
+	local myswap
+	myswap=$(awk '/file/ || /partition/ {print $1}' /proc/swaps)
+	[[ "$myswap" ]] && return "$TRUE" || return "$FALSE"
+}
+
+dd_swapfile() {
+	local -ri size=$(($1 * 1048576))
+	printf "Creating swap file in the root directory...\n"
+	sudo_login 2
+	sudo dd if=/dev/zero of=/swapfile bs=1024 count="$size"
+	return "$?"
+}
+
+fallocate_swapfile() {
+	local -ri size="$1"
+	printf "Creating swap file in the root directory...\n"
+	sudo_login 2
+	sudo fallocate -l "$size"G /swapfile
+	return "$?"
+}
+
+process_swapfile() {
+	ls -lh /swapfile 										# see the file in the root directory
+	sudo e4defrag /swapfile 								# defrag swapfile so it's 1 contigous file
+	sudo chmod 600 /swapfile 								# set file permissions
+	sudo mkswap /swapfile 									# set up swap area
+	sudo swapon /swapfile 									# enable swap file
+	sudo tee -a /etc/fstab <<< "/swapfile none swap sw 0 0"	# add swapfile to /etc/fstab
+	sudo systemctl daemon-reload 							# reload daemons
+	sudo findmnt --verify 									# verify fstab
+	return "$?"
 }
 
 create_swapfile() {
@@ -42,48 +70,25 @@ create_swapfile() {
 	PS3="Choose creation method: "
 	select _opt in "${options[@]}"; do
 		case "$REPLY" in
-			1 ) dd_swapfile "$size"; break ;;
-			2 ) fallocate_swapfile "$size"; break ;;
-			3 ) printf "Swap file creation canceled.\n"; break ;;
-			* ) printf "%sInvalid choice. Try again.%s\n" "$orange" "$normal" >&2
+			1 )
+				dd_swapfile "$size" && process_swapfile
+				break ;;
+			2 )
+				fallocate_swapfile "$size" && process_swapfile
+				break ;;
+			3 )
+				printf "Swap file creation canceled.\n"
+				break ;;
+			* )
+				printf "%sInvalid choice. Try again.%s\n" "$orange" "$normal" >&2
 		esac
 	done
 	return "$?"
 }
 
-dd_swapfile() {
-	local size=$(($1 * 1048576))
-	printf "Creating swap file in the root directory...\n"
-	sudo_login 2
-	sudo dd if=/dev/zero of=/swapfile bs=1024 count="$size"
-	process_swapfile
-	return "$?"
-}
-
-fallocate_swapfile() {
-	local size="$1"
-	printf "Creating swap file in the root directory...\n"
-	sudo_login 2
-	sudo fallocate -l "$size"G /swapfile
-	process_swapfile
-	return "$?"
-}
-
-process_swapfile() {
-	ls -lh /swapfile  																			# see the file in the root directory
-	sudo e4defrag /swapfile																	# defrag swapfile so it's 1 contigous file
-	sudo chmod 600 /swapfile																# set file permissions
-	sudo mkswap /swapfile																		# set up swap area
-	sudo swapon /swapfile																		# enable swap file
-	sudo tee -a /etc/fstab <<< "/swapfile none swap sw 0 0"	# add swapfile to /etc/fstab
-	sudo systemctl daemon-reload														# reload daemons
-	sudo findmnt --verify																		# verify fstab
-	return "$?"
-}
-
 main() {
 	local script="${0##*/}"
-	local version="2.3.26212"
+	local version="2.4.26252"
 	local -i exit_code=0
 	sudo_login 2
 	if swap_exists; then
@@ -95,8 +100,8 @@ main() {
 	fi
 	printf "Current Swap:\n"
 	sudo swapon --show
-  over_line "$script $version"
-  exit "$exit_code"
+	over_line "$script $version"
+	exit "$exit_code"
 }
 
 main "$@"
