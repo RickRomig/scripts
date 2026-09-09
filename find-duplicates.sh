037e1106eb861370915f1d7a58340e38e120a8b8
Richard Romig <rick.romig@gmail.com>
2026-09-09 09:53:04 -0400

    find-duplicates.sh: updated indentation

--- find-duplicates.sh
+++ find-duplicates.sh
@@ -7,8 +7,8 @@
 # Author       : Copyright © 2026, Richard B. Romig, Mosfanet
 # Email        : rick.romig@gmail | rick.romig@mymetronet.net
 # Created      : 19 Feb 2026
-# Updated      : 25 Aug 2026
-# Version      : 0.3.26237 (ALPHA)
+# Updated      : 09 Sep 2026
+# Version      : 0.4.26252 (ALPHA)
 # Comments     :
 # TODO (Rick)  : Still needs work.
 # License      : GNU General Public License, version 2.0
@@ -29,35 +29,35 @@ source ~/bin/functionlib.bash || { printf "\e[91mERROR:\e[0m Unable to source fu
 
 help() {
 	local -r script="$1"
-  local -r version="$2"
-  local -ri errcode="${3:-1}"
-	local -r updated="03 Aug 2026"
-  cat << _HELP_
+	local -r version="$2"
+	local -ri errcode="${3:-1}"
+	local -r updated="09 Sep 2026"
+	cat << _HELP_
 	${orange}$script${normal} $version, Updated: $updated
 	Run this script from in the directory you want to find dupliate files.
 	Have a second terminal window open in which to compare suspected duplicate.
 	Remove duplicates as needed.
 _HELP_
-  exit "$errcode"
+	exit "$errcode"
 }
 
 find_filter_duplicates() {
-  # find file sizes
-  awk '{print $5, $9}' < <(find . -type f -size +1M -exec ls -l {} \;) | sort -n > "$TMP_FILE"
-  # filter duplicates
-  awk 'NR==FNR{count[$1]++; next} count[$1]>1' "$TMP_FILE" "$TMP_FILE" | less -N
-  return "$?"
+	# find file sizes
+	awk '{print $5, $9}' < <(find . -type f -size +1M -exec ls -l {} \;) | sort -n > "$TMP_FILE"
+	# filter duplicates
+	awk 'NR==FNR{count[$1]++; next} count[$1]>1' "$TMP_FILE" "$TMP_FILE" | less -N
+	return "$?"
 }
 
 main() {
-  local -r script="${0##*/}"
-  local -r version="0.3.26237 (ALPHA)"
-  local -i exit_code=0
-  [[ "$1" == "-h" || "$1" == "--help" ]] && help "$script" "$version" 0
-  create_tmp "file"
-  find_filter_duplicates
-  exit_code="$?"
-  over_line "$script $version"
+	local -r script="${0##*/}"
+	local -r version=" 0.4.26252 (ALPHA)"
+	local -i exit_code=0
+	[[ "$1" == "-h" || "$1" == "--help" ]] && help "$script" "$version" 0
+	create_tmp "file"
+	find_filter_duplicates
+	exit_code="$?"
+	over_line "$script $version"
 	exit "$exit_code"
 }
 
