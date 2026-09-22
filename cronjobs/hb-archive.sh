4cfd71bd1866c8e9b68260a36ef27cc0f3710ca6
Richard Romig <rick.romig@gmail.com>
2026-09-21 22:28:22 -0400

    cronjobs/hb-archive.sh: updated local variables

--- cronjobs/hb-archive.sh
+++ cronjobs/hb-archive.sh
@@ -1,56 +1,55 @@
 #!/usr/bin/env bash
-##########################################################################
+###############################################################################
 # Script Name  : hb-archive.sh
 # Description  : Create HomeBank archive of *.bak files as a monthly cron job.
 # Dependencies : zip
 # Arguments    : none
 # Author       : Copyright (C) 2020, Richard B. Romig, 21 January 2020
 # Email        : rick.romig@gmail.com | rick.romig@mymetronet.net
-# Last updated : 16 Apr 2026
-# Version      : 5.2.26248
+# Last updated : 21 Sep 2026
+# Version      : 5.3.26264
 # Comments     : Run from user's crontab to run on the 1st of the month
 #              : to archive 2nd month previous. (1 May archives March files)
 # License      : GNU General Public License, version 2.0
 # License URL  : https://github.com/RickRomig/scripts/blob/main/LICENSE
-##########################################################################
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
-# (at your option) any later version.
+###############################################################################
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 2 of the License, or (at your option) any later
+# version.
 #
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-# GNU General Public License for more details.
-##########################################################################
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+###############################################################################
 
 # If log files contains more than 12 entries, trim top entry.
 trim_log() {
-	local log_len log_dir log_file
-	log_dir="$1"
-	log_file="$2"
+	local -r log_dir="$1"
+	local -r log_file="$2"
+	local log_len
 	log_len=$(wc -l < "$log_dir/$log_file")
 	[[ "$log_len" -gt 12 ]] && sed -i '1d' "$log_dir/$log_file"
 }
 
 # Delete archive files older than 3 years.
 del_old_archives() {
-	local arc_dir="$1"
+	local -r arc_dir="$1"
 	find "$arc_dir" -daystart -mtime +1095 -exec rm {} +
 }
 
 # Backup *.bak files from from 2 months previous.
 archive_bak() {
-	local arc_date ref_date
-	arc_date=$(date -d "$(date +%Y-%m-01) - 2 months" +%Y-%m)
-	ref_date=$(date -d "$(date +%Y-%m-01) - 1 month" +%m%d%Y)
 	local -r arc_dir=~/Downloads/archives/homebank
 	local -r log_dir=~/.local/share/logs
-	local -r log_file="HomeBank-archive.log"
-	local -r archive="$arc_date-backup.zip"
+	local -r log_file=HomeBank-archive.log
 	local -r hb_dir=~/Documents/HomeBank
-	local -r err_log="HomeBank-error.log"
-	local status=0
+	local -r err_log=HomeBank-error.log
+	local -i status=0
+	local archive arc_date ref_date
+	arc_date=$(date -d "$(date +%Y-%m-01) - 2 months" +%Y-%m)
+	ref_date=$(date -d "$(date +%Y-%m-01) - 1 month" +%m%d%Y)
+	archive="$arc_date-backup.zip"
 	# Create directories if they don't exist.
 	[[ -d "$log_dir" ]] || mkdir -p "$log_dir"
 	[[ -d "$arc_dir" ]] || mkdir -p "$arc_dir"
