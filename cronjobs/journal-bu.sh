9947b82c1e647c8e9228bf386ee2189bfd2c6c7c
Richard Romig <rick.romig@gmail.com>
2026-09-21 22:36:49 -0400

    cronjobs/journal-bu.sh: updated local variables

--- cronjobs/journal-bu.sh
+++ cronjobs/journal-bu.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-##########################################################################
+###############################################################################
 # Script Name  : journal-bu.sh
 # Description  : Backup personal journals
 # Dependencies : none
@@ -7,42 +7,40 @@
 # Author       : Copyright © 2024 Richard B. Romig, Mosfanet
 # Email        : rick.romig@gmail.com | rick.romig@mymetronet.net
 # Created      : 11 Mar 2024
-# Last updated : 09 Oct 2025
-# Version      : 2.4.25282
+# Last updated : 21 Sep 2026
+# Version      : 2.5.26264
 # Comments     : Run as a local cron job.
 # TODO (Rick)  :
 # License      : GNU General Public License, version 2.0
 # License URL  : https://github.com/RickRomig/scripts/blob/main/LICENSE
-##########################################################################
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
-# (at your option) any later version.
+###############################################################################
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 2 of the License, or (at your option) any later
+# version.
 #
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-# GNU General Public License for more details.
-##########################################################################
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+###############################################################################
 
 monday_actions() {
-	local arc_dir="$1"
-	local snar="$2"
+	local -r arc_dir="$1"
+	local -r snar="$2"
 	[[ -e "$arc_dir/$snar" ]] && mv "$arc_dir/$snar" "$arc_dir/$snar.$(date --date '7 days ago' +%y%m%d)"
-	find "$arc_dir" -mtime +91 -delete
+	find "$arc_dir" -mtime +364 -delete
 }
 
 incremental_backup() {
-	local arc_dir archive day snar
-	arc_dir=~/Downloads/archives/journals
+	local -r arc_dir=~/Downloads/archives/journals
+	local -r snar="journals.snar"
+	local archive day
 	archive="journals.$(date +'%y%m%d-%u').tar.gz"
-	snar="journals.snar"
 	day=$(date +%a)
 	[[ -d "$arc_dir" ]] || mkdir -p "$arc_dir"
 	[[ "$day" == "Mon" ]] && monday_actions "$arc_dir" "$snar"
 	tar -cpzg "$arc_dir/$snar" -f "$arc_dir/$archive" --exclude='.~lock*' -C ~/Documents Journals
 	rsync -aq --delete ~/Downloads/archives/journals/ rick@192.168.0.11:/data/archives/journals/
-
 }
 
 main() {
