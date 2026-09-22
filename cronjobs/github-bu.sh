65abd76df04eb59f0972a83fb9c408b3f25bf1a5
Richard Romig <rick.romig@gmail.com>
2026-09-21 22:20:36 -0400

    cronjobs/github-bu.sh: updated local variables

--- cronjobs/github-bu.sh
+++ cronjobs/github-bu.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-##########################################################################
+###############################################################################
 # Script Name  : github-bu.sh
 # Description  : Incremental backups of GitHub (Projects) repositories
 # Dependencies : none
@@ -7,47 +7,41 @@
 # Author       : Copyright © 2023 Richard B. Romig, Mosfanet
 # Email        : rick.romig@gmail.com | rick.romig@mymetronet.net
 # Created      : 26 Oct 2023
-# Last updated : 19 Jul 2025
-# Version      : 2.2.25200
+# Last updated : 21 Sep 2026
+# Version      : 2.3.26264
 # Comments     : Run as a daily cron job on the main system.
+#              : Replaced by git-bu.sh
 # TODO (Rick)  :
 # License      : GNU General Public License, version 2.0
 # License URL  : https://github.com/RickRomig/scripts/blob/main/LICENSE
-##########################################################################
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
-# (at your option) any later version.
+###############################################################################
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 2 of the License, or (at your option) any later
+# version.
 #
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-# GNU General Public License for more details.
-##########################################################################
-
-set -eu
-
-## Functions ##
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+###############################################################################
 
 monday_actions() {
-	local arc_dir="$1"
-	local snar="$2"
+	local -r arc_dir="$1"
+	local -r snar="$2"
 	[[ -e "$arc_dir/$snar" ]] && mv "$arc_dir/$snar" "$arc_dir/$snar.$(date --date '7 days ago' +%y%m%d)"
 	find "$arc_dir" -mtime +91 -delete
 }
 
 main() {
-	local arc_dir archive day snar
-	arc_dir="$HOME/Downloads/archives/projects-repo"
+	local -r arc_dir=~/Downloads/archives/projects-repo
+	local -r snar="github.snar"
+	local archive day
 	archive="github.$(date +'%y%m%d-%u').tar.gz"
-	snar="github.snar"
 	day=$(date +%a)
 	[[ -d "$arc_dir" ]] || mkdir -p "$arc_dir"
 	[[ "$day" == "Mon" ]] && monday_actions "$arc_dir" "$snar"
 	tar -cpzg "$arc_dir/$snar" -f "$arc_dir/$archive" -C "$HOME" Projects
 	exit
 }
-
-## Execution ##
 
 main "$@"
