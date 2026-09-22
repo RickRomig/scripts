f73854b0d8a30fed96f7f8e62d07bdc81c3c7bc1
Richard Romig <rick.romig@gmail.com>
2026-09-21 21:46:48 -0400

    cronjobs/fin-bu.sh: updated local variables

--- cronjobs/fin-bu.sh
+++ cronjobs/fin-bu.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-##########################################################################
+###############################################################################
 # Script Name  : fin-bu.sh
 # Description  : Incremental backups of Finance & HomeBank directories
 # Dependencies : none
@@ -7,38 +7,35 @@
 # Author       : Copyright © 2023 Richard B. Romig, Mosfanet
 # Email        : rick.romig@gmail.com | rick.romig@mymetronet.net
 # Created      : 28 Oct 2023
-# Last updated : 09 Oct 2025
-# Version      : 3.0.25282
+# Last updated : 21 Sep 2026
+# Version      : 3.1.26264
 # Comments     : Run as a local daily cron job
 # TODO (Rick)  :
 # License      : GNU General Public License, version 2.0
 # License URL  : https://github.com/RickRomig/scripts/blob/main/LICENSE
-##########################################################################
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
-# (at your option) any later version.
+###############################################################################
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 2 of the License, or (at your option) any later
+# version.
 #
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-# GNU General Public License for more details.
-##########################################################################
-
-## Functions ##
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+###############################################################################
 
 monday_actions() {
-	local arc_dir="$1"
-	local snar="$2"
+	local -r arc_dir="$1"
+	local -r snar="$2"
 	[[ -e "$arc_dir/$snar" ]] && mv "$arc_dir/$snar" "$arc_dir/$snar.$(date --date '7 days ago' +%y%m%d)"
 	find "$arc_dir" -mtime +91 -delete
 }
 
 incremental_backup() {
-	local arc_dir archive day snar
-	arc_dir=~/Downloads/archives/finance
+	local -r arc_dir=~/Downloads/archives/finance
+	local -r snar=finance.snar
+	local archive day
 	archive="finance.$(date +'%y%m%d-%u').tar.gz"
-	snar="finance.snar"
 	day=$(date +%a)
 	[[ -d "$arc_dir" ]] || mkdir -p "$arc_dir"
 	[[ "$day" == "Mon" ]] && monday_actions "$arc_dir" "$snar"
@@ -49,7 +46,5 @@ main() {
 	incremental_backup
 	exit
 }
-
-## Execution ##
 
 main "$@"
